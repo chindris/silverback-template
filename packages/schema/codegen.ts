4210a188507d911843fa172c9145a1522f74e1ec
Philipp Melab <philipp.melab@gmail.com>
2023-03-22 14:21:37 +0100

    feat: graphql schema and typings package

---
+++ packages/schema/codegen.ts
@@ -0,0 +1,44 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  schema: 'src/schema.graphqls',
+  documents: ['src/fragments/**/*.gql', 'src/operations/**/*.gql'],
+  generates: {
+    // Persisted query ids to be consumed by Drupal.
+    'build/operations.json': {
+      plugins: ['@amazeelabs/codegen-operation-ids'],
+    },
+    // All fragments collected into one javascript file for Gatsby.
+    'build/gatsby-fragments.js': {
+      plugins: ['@amazeelabs/codegen-gatsby-fragments'],
+    },
+    // The main entry point. Contains:
+    // - All types generated from the schema.
+    // - All operation types
+    // - All fragment types
+    // - All operations as typed id's
+    'build/index.ts': {
+      plugins: [
+        {
+          typescript: {
+            enumsAsTypes: true,
+          },
+        },
+        `typescript-operations`,
+        '@amazeelabs/codegen-operation-ids',
+      ],
+      config: {
+        maybeValue: 'T | undefined',
+        strictScalars: true,
+        scalars: {
+          Markup: '@amazeelabs/scalars#Markup',
+          Url: '@amazeelabs/scalars#Url',
+          ImageSource: '@amazeelabs/scalars#ImageSource',
+        },
+        withObjectType: true,
+      },
+    },
+  },
+};
+
+export default config;
