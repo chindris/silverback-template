dd809e015e868cc014e308578f331ea16b50ffab
Philipp Melab <philipp.melab@gmail.com>
2023-04-15 15:59:16 +0200

    refactor: merge cms- and website tests into a single e2e test package
    Both applications will be required for preview testing at once.

---
+++ tests/e2e/specs/login.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { cmsUrl } from '../helpers/url';
+
+test.describe('authentication', () => {
+  test.use({ storageState: '.auth/admin.json' });
+  test('login form', async ({ page }) => {
+    await page.goto(cmsUrl('/user'));
+    await expect(
+      page.getByRole('heading', { name: 'admin', exact: true }),
+    ).toBeVisible();
+  });
+});
---
+++ tests/e2e/specs/setup.ts
@@ -0,0 +1,13 @@
+import { test as setup, expect } from '@playwright/test';
+import { cmsUrl } from '../helpers/url';
+
+setup('setup', async ({ page }) => {
+  await page.goto(cmsUrl('/user/login'));
+  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await expect(
+    page.getByRole('heading', { name: 'admin', exact: true }),
+  ).toHaveCount(1);
+  await page.context().storageState({ path: '.auth/admin.json' });
+});
