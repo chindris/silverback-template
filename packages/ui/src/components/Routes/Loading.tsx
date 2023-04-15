49374315a3cb5933ba51c17f4854a496e58be02c
Philipp Melab <philipp.melab@gmail.com>
2023-04-15 16:00:44 +0200

    feat(ui): add simple loading indicator component

---
+++ packages/ui/src/components/Routes/Loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Loading() {
+  return (
+    <div className="animate-pulse text-xl font-bold italic text-indigo-600 text-center my-8">
+      Loading&hellip;
+    </div>
+  );
+}
