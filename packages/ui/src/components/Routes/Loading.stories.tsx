de50434203302d7fe16e25c249d35200b1b7d419
Philipp Melab <philipp.melab@gmail.com>
2023-04-15 16:00:44 +0200

    feat(ui): add simple loading indicator component

---
+++ packages/ui/src/components/Routes/Loading.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { Loading } from './Loading';
+
+export default {
+  component: Loading,
+} satisfies Meta<typeof Loading>;
+
+export const LoadingStory = {} satisfies StoryObj<typeof Loading>;
