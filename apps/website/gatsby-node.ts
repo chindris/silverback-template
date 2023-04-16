785e9d1893fe173c7ae983c1396caad227dc9fe4
Philipp Melab <philipp.melab@gmail.com>
2023-04-16 07:53:56 +0200

    feat: multilingual home page and redirects

---
+++ apps/website/gatsby-node.ts
@@ -0,0 +1,97 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { GatsbyNode } from 'gatsby';
+import type { SilverbackPageContext } from '@amazeelabs/gatsby-source-silverback';
+
+export const createSchemaCustomization: GatsbyNode['createSchemaCustomization'] =
+  (args) => {
+    // TODO: This is still necessary, because graphql-source-toolkit won't import
+    //       interface relations.
+    const schema = readFileSync(
+      `./node_modules/@custom/schema/src/schema.graphqls`,
+      'utf8',
+    ).toString();
+    args.actions.createTypes(schema);
+  };
+
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = ({
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@amazeelabs/bridge': resolve(__dirname, '/src/bridge/'),
+      },
+    },
+  });
+};
+
+export const createPages: GatsbyNode['createPages'] = async ({
+  actions,
+  graphql,
+}) => {
+  const settings = await graphql<{
+    websiteSettings?: {
+      homePage?: {
+        translations: Array<{
+          typeName: string;
+          path: string;
+          locale: string;
+          id: string;
+          remoteId: string;
+        }>;
+      };
+    };
+  }>(`
+    query IndexPages {
+      websiteSettings {
+        homePage {
+          translations {
+            typeName: __typename
+            locale
+            id
+            remoteId
+            path
+          }
+        }
+      }
+    }
+  `);
+
+  if (settings.errors) {
+    throw settings.errors;
+  }
+
+  if (settings.data?.websiteSettings?.homePage) {
+    const frontPageLocalizations =
+      settings.data?.websiteSettings?.homePage.translations.map(
+        ({ locale }) => ({
+          path: `/${locale}`,
+          locale,
+        }),
+      ) satisfies SilverbackPageContext['localizations'];
+
+    settings.data?.websiteSettings?.homePage.translations.forEach(
+      ({ locale, typeName, id, remoteId, path }) => {
+        actions.createPage({
+          path: `/${locale}`,
+          component: resolve(`./src/templates/page.tsx`),
+          context: {
+            typeName,
+            id,
+            remoteId,
+            locale,
+            localizations: frontPageLocalizations,
+          } satisfies SilverbackPageContext,
+        });
+
+        actions.createRedirect({
+          fromPath: path,
+          toPath: `/${locale}`,
+          isPermanent: true,
+          force: true,
+        });
+      },
+    );
+  }
+};
