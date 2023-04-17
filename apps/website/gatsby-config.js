d200b3ad2854b0eee57017b858dbfb70cd1b2984
Alex Tkachev <alex.leksat@gmail.com>
2023-04-17 20:50:34 +0300

    chore: make tests work locally
    Somehow node is not able to access http://localhost:8888

--- apps/website/gatsby-config.js
+++ apps/website/gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: '@amazeelabs/gatsby-source-silverback',
       options: {
-        drupal_url: process.env.DRUPAL_INTERNAL_URL || 'http://localhost:8888',
+        drupal_url: process.env.DRUPAL_INTERNAL_URL || 'http://127.0.0.1:8888',
         graphql_path: '/graphql',
         auth_key: 'cfdb0555111c0f8924cecab028b53474',
         type_prefix: '',
