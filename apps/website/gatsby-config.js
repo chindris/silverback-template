b6178193deb6b38e914f3b931499de2a92874ec9
Philipp Melab <philipp.melab@gmail.com>
2023-04-19 20:07:11 +0200

    ci: proxy images throught netlify
    Necessary because cloudinary mocking is subject to CORS.

--- apps/website/gatsby-config.js
+++ apps/website/gatsby-config.js
@@ -11,6 +11,10 @@ process.env.CLOUDINARY_CLOUDNAME = process.env.CLOUDINARY_CLOUDNAME || 'demo';
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  proxy: {
+    prefix: '/sites/default/files',
+    url: process.env.DRUPAL_EXTERNAL_URL || 'http://localhost:8888',
+  },
   flags: {
     PARTIAL_HYDRATION: true,
   },
@@ -19,6 +23,9 @@ module.exports = {
       resolve: '@amazeelabs/gatsby-source-silverback',
       options: {
         drupal_url: process.env.DRUPAL_INTERNAL_URL || 'http://127.0.0.1:8888',
+        drupal_external_url:
+          // File requests are proxied through netlify.
+          process.env.NETLIFY_URL || 'http://localhost:8000',
         graphql_path: '/graphql',
         auth_key: 'cfdb0555111c0f8924cecab028b53474',
         type_prefix: '',
