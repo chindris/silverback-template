b9d0f628d1bc831ec023b356245345ca77b71000
Philipp Melab <philipp.melab@gmail.com>
2023-04-19 20:08:27 +0200

    feat: cloudinary default variables that work for local

--- apps/website/gatsby-config.js
+++ apps/website/gatsby-config.js
@@ -1,6 +1,12 @@
 process.env.GATSBY_DRUPAL_URL =
   process.env.DRUPAL_EXTERNAL_URL || 'http://localhost:8888';
 
+process.env.NETLIFY_URL = process.env.NETLIFY_URL || 'http://localhost:8000';
+
+process.env.CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || 'test';
+process.env.CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || 'test';
+process.env.CLOUDINARY_CLOUDNAME = process.env.CLOUDINARY_CLOUDNAME || 'demo';
+
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
@@ -16,6 +22,12 @@ module.exports = {
         graphql_path: '/graphql',
         auth_key: 'cfdb0555111c0f8924cecab028b53474',
         type_prefix: '',
+      },
+    },
+    {
+      resolve: '@amazeelabs/gatsby-silverback-cloudinary',
+      options: {
+        responsiveImageFields: 'MediaImage.source',
       },
     },
     {
