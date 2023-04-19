d3cec93cd769ac354701e5d3ea2294e4b298bf29
Philipp Melab <philipp.melab@gmail.com>
2023-04-19 20:06:00 +0200

    fix: adapt ui package to new schema and typings

--- packages/ui/src/components/Organisms/PageContent/BlockImage.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockImage.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 export function BlockImage(props: BlockImageFragment) {
   return (
     <figure className="mt-16 mx-auto max-w-3xl">
-      <Image
-        className="aspect-video rounded-xl object-cover w-full"
-        source={props.source}
-        alt={props.alt}
-      />
+      {props.image ? (
+        <Image
+          className="aspect-video rounded-xl object-cover w-full"
+          source={props.image.source}
+          alt={props.alt}
+        />
+      ) : null}
       {props.caption ? (
         <figcaption className="mt-4 flex gap-x-2 text-sm leading-6 text-gray-500">
           <Html markup={props.caption} />
