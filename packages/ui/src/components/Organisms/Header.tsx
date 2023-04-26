314be368530879a0e0a22da361485222a17f35c1
Philipp Melab <philipp.melab@gmail.com>
2023-04-26 17:41:14 +0200

    fix: fixup

--- packages/ui/src/components/Organisms/Header.tsx
+++ packages/ui/src/components/Organisms/Header.tsx
@@ -1,5 +1,4 @@
 import { Link, NavigationFragment } from '@custom/schema';
-import { Disclosure } from '@headlessui/react';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 
