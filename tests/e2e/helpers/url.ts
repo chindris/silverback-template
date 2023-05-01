33efabe2f75a80f92e7da7edd18d8c55865e5524
Alex Tkachev <alex.leksat@gmail.com>
2023-05-01 10:23:34 +0300

    chore: always use 127.0.0.1

--- tests/e2e/helpers/url.ts
+++ tests/e2e/helpers/url.ts
@@ -1,9 +1,9 @@
 export function websiteUrl(path: string): string {
   return `${
-    process.env.PLAYWRIGHT_WEBSITE_URL || 'http://localhost:8000'
+    process.env.PLAYWRIGHT_WEBSITE_URL || 'http://127.0.0.1:8000'
   }${path}`;
 }
 
 export function cmsUrl(path: string): string {
-  return `${process.env.PLAYWRIGHT_CMS_URL || 'http://localhost:8888'}${path}`;
+  return `${process.env.PLAYWRIGHT_CMS_URL || 'http://127.0.0.1:8888'}${path}`;
 }
