741ba43d7aaeeaf9133e384cbefe5144078cf986
Alex Tkachev <alex.leksat@gmail.com>
2023-05-04 13:43:16 +0300

    chore: improve content editing experience

---
+++ tests/e2e/specs/content-editing.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@playwright/test';
+
+import { cmsUrl } from '../helpers/url';
+
+test.describe('content-editing', () => {
+  test.use({ storageState: '.auth/admin.json' });
+
+  test('moderation controls are placed in the sidebar', async ({ page }) => {
+    await page.goto(cmsUrl('/node/add/page'));
+    await expect(
+      page.locator('[aria-label="Editor settings"]').getByText('Save as'),
+    ).toBeVisible();
+  });
+
+  test('"More settings" fieldset is removed', async ({ page }) => {
+    // Why we expect it to be removed:
+    // - It's too long to scroll to the bottom of long pages
+    // - If we have any valuable controls in the "More settings" fieldset,
+    //   we should move them to the sidebar, where they are much easier to
+    //   access
+    await page.goto(cmsUrl('/node/add/page'));
+    await expect(page.locator(':text-is("More settings")')).toHaveCount(0);
+  });
+});
