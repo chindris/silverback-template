628d9f467d23a3c3f07830ebd4dbc117065d538c
Alex Tkachev <alex.leksat@gmail.com>
2023-05-17 14:38:31 +0300

    chore: assert response code as well

--- tests/e2e/specs/redirects.spec.ts
+++ tests/e2e/specs/redirects.spec.ts
@@ -4,7 +4,8 @@ import { websiteUrl } from '../helpers/url';
 
 test.describe('drupal redirects', () => {
   test('are detected', async ({ page }) => {
-    await page.goto(websiteUrl('/node/1'));
+    const response = await page.goto(websiteUrl('/node/1'));
+    await expect(response.status()).toBe(200);
     await expect(page.url).not.toBe(websiteUrl('/node/1'));
   });
 });
