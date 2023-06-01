77b95bc64f51f598076fa5fcd71bb61c293d3533
Philipp Melab <philipp.melab@gmail.com>
2023-06-01 10:30:37 +0000

    feat(content-hub): schema definitions for content hub

---
+++ tests/schema/specs/content-hub.spec.ts
@@ -0,0 +1,66 @@
+import gql from 'noop-tag';
+import { describe, expect, it } from 'vitest';
+
+import { fetch } from './lib';
+
+describe('content hub', () => {
+  it('returns limited results', async () => {
+    const result = await fetch(gql`
+      {
+        contentHub(pagination: { limit: 2, offset: 0 }) {
+          items {
+            title
+          }
+          total
+        }
+      }
+    `);
+    expect(result.data.contentHub.total).toBe(12);
+    expect(result.data.contentHub.items.length).toBe(2);
+  });
+
+  it('respects an offset', async () => {
+    const resultA = await fetch(gql`
+      {
+        contentHub(pagination: { limit: 2, offset: 0 }) {
+          items {
+            path
+          }
+        }
+      }
+    `);
+    const resultB = await fetch(gql`
+      {
+        contentHub(pagination: { limit: 2, offset: 1 }) {
+          items {
+            path
+          }
+        }
+      }
+    `);
+
+    expect(resultA.data.contentHub.items[0].path).not.toBe(
+      resultB.data.contentHub.items[0].path,
+    );
+    expect(resultA.data.contentHub.items[1].path).not.toBe(
+      resultB.data.contentHub.items[1].path,
+    );
+    expect(resultA.data.contentHub.items[1].path).toBe(
+      resultB.data.contentHub.items[0].path,
+    );
+  });
+
+  it('accepts a query for the title', async () => {
+    const result = await fetch(gql`
+      {
+        contentHub(query: "Arch", pagination: { limit: 3, offset: 0 }) {
+          total
+          items {
+            path
+          }
+        }
+      }
+    `);
+    expect(result.data.contentHub.items[0].path).toBe('/en/architecture');
+  });
+});
