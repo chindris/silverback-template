8911e2adf6ff0984ecbf51e95e2d4d0a530fe3ba
Philipp Melab <philipp.melab@gmail.com>
2023-06-01 10:50:58 +0000

    feat(content-hub): pagination molecule

---
+++ packages/ui/src/components/Molecules/Pagination.stories.ts
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+
+import { Pagination as Component } from './Pagination';
+
+export default {
+  component: Component,
+} satisfies Meta<typeof Component>;
+
+export const Initial: StoryObj<typeof Component> = {
+  args: {
+    total: 82,
+    pageSize: 10,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await canvas.findByText('1 of 9');
+  },
+};
+
+export const Preselected: StoryObj<typeof Component> = {
+  args: {
+    total: 82,
+    pageSize: 10,
+  },
+  parameters: {
+    location: new URL('http://localhost/?page=2'),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await canvas.findByText('2 of 9');
+  },
+};
+
+export const Interactive: StoryObj<typeof Component> = {
+  args: {
+    total: 82,
+    pageSize: 10,
+  },
+  parameters: {
+    location: new URL('http://localhost/?page=1'),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByText('Next'));
+    await canvas.findByText('2 of 9');
+    await userEvent.click(canvas.getByText('Next'));
+    await canvas.findByText('3 of 9');
+    await userEvent.click(canvas.getByText('Previous'));
+    await canvas.findByText('2 of 9');
+  },
+};
