64c56c09fe2caa735a19a70fdc624d02534abcd7
Philipp Melab <philipp.melab@gmail.com>
2023-06-01 10:13:52 +0000

    feat(react): pass reverse proxy headers to client side requests
    Use the new `SLB-Forwarded-*` headers to make Drupal emit correct image
    urls.

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -1,7 +1,9 @@
+import useSwr from 'swr';
+
+import { AnyOperationId, OperationResult, OperationVariables } from '../build';
+
 export * from '../build';
 export * from '@amazeelabs/scalars';
-import useSwr from 'swr';
-import { AnyOperationId, OperationResult, OperationVariables } from '../build';
 
 export function useOperation<TOperation extends AnyOperationId>(
   endpoint: string,
@@ -12,11 +14,16 @@ export function useOperation<TOperation extends AnyOperationId>(
     [operation, variables],
     variables
       ? () => {
-          const url = new URL(endpoint);
+          const url = new URL(endpoint, window.location.origin);
           url.searchParams.set('queryId', operation);
           url.searchParams.set('variables', JSON.stringify(variables));
           return fetch(url, {
             credentials: 'include',
+            headers: {
+              'SLB-Forwarded-Proto': window.location.protocol.slice(0, -1),
+              'SLB-Forwarded-Host': window.location.hostname,
+              'SLB-Forwarded-Port': window.location.port,
+            },
           }).then((r) => r.json());
         }
       : null,
