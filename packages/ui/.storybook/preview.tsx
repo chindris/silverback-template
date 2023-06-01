6ab49ea47d2a4c35bbe042c444c1c1de96bb7594
Philipp Melab <philipp.melab@gmail.com>
2023-06-01 10:40:03 +0000

    ci: introduce mock service worker for testing client side queries
    This requires to switch the cloudinary-mocks to integrate into msw,
    since only one servicer worker can be active at a time.

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -2,6 +2,9 @@ import '../tailwind.css';
 
 import { Decorator } from '@storybook/react';
 import { IntlProvider } from '../src/utils/intl';
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+import { rest } from 'msw';
+import { mockCloudinaryImage } from '@amazeelabs/cloudinary-responsive-image';
 
 // Every story is wrapped in an IntlProvider by default.
 const IntlDecorator: Decorator = (Story) => (
@@ -12,6 +15,22 @@ const IntlDecorator: Decorator = (Story) => (
 
 export const parameters = {
   chromatic: { viewports: [320, 840, 1440] },
+  msw: {
+    handlers: {
+      cloudinary: [
+        rest.get('https://res.cloudinary.com/*', async (req, res, context) => {
+          return res(
+            context.set('Content-Type', 'image/jpg'),
+            context.body((await mockCloudinaryImage(req.url.toString())) || ''),
+          );
+        }),
+      ],
+    },
+  },
 };
 
-export const decorators = [IntlDecorator];
+initialize({
+  onUnhandledRequest: 'bypass',
+});
+
+export const decorators = [IntlDecorator, mswDecorator];
