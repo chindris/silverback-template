b465454543ea3e77840ae9506648ad2daa8c48d9
Philipp Melab <philipp.melab@gmail.com>
2023-06-01 14:43:28 +0000

    feat(preview): share cookie between backend and frontend

---
+++ packages/drupal/custom/src/CustomSessionConfiguration.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\custom;
+
+use Drupal\Core\Session\SessionConfiguration;
+use Drupal\Core\Session\SessionConfigurationInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Decorator for the core session configuration service.
+ */
+class CustomSessionConfiguration extends SessionConfiguration {
+
+  /* @var \Drupal\Core\Session\SessionConfigurationInterface */
+  protected $sessionConfigurationInner;
+
+  /**
+   * Creates a CaritasSessionConfiguration object.
+   */
+  public function __construct(SessionConfigurationInterface $session_configuration_inner, $options = []) {
+    $this->sessionConfigurationInner = $session_configuration_inner;
+    parent::__construct($options);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function getOptions(Request $request) {
+    $options = parent::getOptions($request);
+    if ($request->isSecure()) {
+      $options['cookie_samesite'] = 'None';
+    }
+    return $options;
+  }
+
+}
