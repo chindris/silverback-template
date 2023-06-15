5b2f876f9d28cd810b5f8092dcb2c283e7d5ca16
Vasile Chindris <vasile.chindris@amazee.com>
2023-06-15 09:14:09 +0300

    chore: enabled the silverback_campaign_urls module and added e2e tests for it

---
+++ tests/e2e/specs/campaign-urls.spec.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@playwright/test';
+
+import { websiteUrl } from '../helpers/url';
+
+test.describe('campaign url redirects', () => {
+  test('simple redirects', async ({ page }) => {
+    await page.goto(websiteUrl('/php_redirect'));
+    await expect(page).toHaveURL(websiteUrl('/en/php'));
+
+    await page.goto(websiteUrl('/prvtsphaere'));
+    await expect(page).toHaveURL(websiteUrl('/de/privatsphaere'));
+  });
+
+  test('splat redirects', async ({ page }) => {
+    await page.goto(websiteUrl('/old_tree/oak'));
+    await expect(page).toHaveURL(websiteUrl('/en/tree/oak'));
+
+    await page.goto(websiteUrl('/old_tree/maple'));
+    await expect(page).toHaveURL(websiteUrl('/en/tree/maple'));
+
+    await page.goto(websiteUrl('/old_tree/cherry'));
+    await expect(page).toHaveURL(websiteUrl('/en/tree/cherry'));
+  });
+});
