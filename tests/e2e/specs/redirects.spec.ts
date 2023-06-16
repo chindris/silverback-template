5cdc19f2bfdd3f471f80a32c4bf573d8864a5944
Alex Tkachev <alex.leksat@gmail.com>
2023-06-16 14:02:01 +0300

    test: static tests for e2e tests
    Each second word is test 🙈

--- tests/e2e/specs/redirects.spec.ts
+++ tests/e2e/specs/redirects.spec.ts
@@ -5,7 +5,7 @@ import { websiteUrl } from '../helpers/url';
 test.describe('drupal redirects', () => {
   test('are detected', async ({ page }) => {
     const response = await page.goto(websiteUrl('/node/1'));
-    await expect(response.status()).toBe(200);
+    await expect(response?.status()).toBe(200);
     await expect(page.url).not.toBe(websiteUrl('/node/1'));
   });
 });
