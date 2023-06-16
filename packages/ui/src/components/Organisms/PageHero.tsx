0f9a43d4b39784eab4e75f589d2a56886bcc5668
Alex Tkachev <alex.leksat@gmail.com>
2023-06-16 14:34:51 +0300

    fix: let page have h1

--- packages/ui/src/components/Organisms/PageHero.tsx
+++ packages/ui/src/components/Organisms/PageHero.tsx
@@ -38,9 +38,9 @@ export function PageHero(props: NonNullable<PageFragment['hero']>) {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
             {props.headline}
-          </h2>
+          </h1>
           {props.lead ? (
             <p className="mt-6 text-lg leading-8 text-gray-300">{props.lead}</p>
           ) : null}
