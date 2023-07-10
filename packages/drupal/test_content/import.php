b5b66e3e5b3f8d8e188940b081ffd58145ad42f0
Alex Tkachev <alex.leksat@gmail.com>
2023-07-10 18:37:04 +0300

    fix: improve content export

--- packages/drupal/test_content/import.php
+++ packages/drupal/test_content/import.php
@@ -12,5 +12,5 @@ Import::run('test_content');
 // Webforms are ignored from the config. Create an example form now.
 var_dump(getcwd());
 Webform::create(Yaml::decode(file_get_contents(
-  'modules/custom/test_content/content/contact-webform.yml'
+  'modules/custom/test_content/contact-webform.yml'
 )))->save();
