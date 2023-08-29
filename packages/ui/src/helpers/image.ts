bfd4be54ebdf5976f03e246a6066b55177392001
Philipp Melab <philipp.melab@gmail.com>
2023-08-29 03:24:14 +0000

    ci(cloudinary): use "local" image versions in lagoon and demo builds
    Gatsby on lagoon and "demo" storybook deploys should use mocked cropping instead of placeholders.

--- packages/ui/src/helpers/image.ts
+++ packages/ui/src/helpers/image.ts
@@ -5,9 +5,9 @@ export function image(
   { src, width, height }: Parameters<typeof buildResponsiveImage>[1],
   config?: Parameters<typeof buildResponsiveImage>[2],
 ) {
-  return buildResponsiveImage(
+  const img = buildResponsiveImage(
     {
-      cloudname: 'demo',
+      cloudname: import.meta.env.VITE_CLOUDINARY_CLOUDNAME || 'placeholder',
       key: 'c7d2fe',
       secret: '4e46e5',
     },
@@ -20,4 +20,5 @@ export function image(
         (config?.width ? config.width * (height / width) : height),
     },
   ) as ImageSource;
+  return img;
 }
