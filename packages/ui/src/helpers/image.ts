1efe4d8d5b64d3c41e2db7392ecd30fba07dc5f8
Philipp Melab <philipp.melab@gmail.com>
2023-09-13 13:44:31 +0000

    refactor: streamline image helper

--- packages/ui/src/helpers/image.ts
+++ packages/ui/src/helpers/image.ts
@@ -5,11 +5,11 @@ export function image(
   { src, width, height }: Parameters<typeof buildResponsiveImage>[1],
   config?: Parameters<typeof buildResponsiveImage>[2],
 ) {
-  const img = buildResponsiveImage(
+  return buildResponsiveImage(
     {
-      cloudname: import.meta.env.VITE_CLOUDINARY_CLOUDNAME || 'placeholder',
-      key: 'c7d2fe',
-      secret: '4e46e5',
+      cloudname: 'test',
+      key: 'test',
+      secret: 'test',
     },
     { src, width, height },
     {
@@ -20,5 +20,4 @@ export function image(
         (config?.width ? config.width * (height / width) : height),
     },
   ) as ImageSource;
-  return img;
 }
