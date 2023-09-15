f067e29c47bbcabcb626e0101c094a00c027f04b
Philipp Melab <philipp.melab@gmail.com>
2023-09-15 09:53:44 +0000

    refactor: use gatsby-plugin-layout instead of wrapper

--- apps/website/src/templates/page.tsx
+++ apps/website/src/templates/page.tsx
@@ -1,10 +1,8 @@
 import { SilverbackPageContext } from '@amazeelabs/gatsby-source-silverback';
-import { Locale, PageFragment } from '@custom/schema';
+import { PageFragment } from '@custom/schema';
 import { Page } from '@custom/ui/routes/Page';
 import { graphql, HeadProps, PageProps } from 'gatsby';
 import React from 'react';
-
-import { Wrapper } from '../utils/wrapper';
 
 export const query = graphql`
   query PageTemplate($remoteId: String!) {
@@ -48,11 +46,6 @@ export function Head({ data }: HeadProps<PageTemplateQuery>) {
 
 export default function PageTemplate({
   data,
-  pageContext,
 }: PageProps<PageTemplateQuery, SilverbackPageContext>) {
-  return (
-    <Wrapper locale={(pageContext.locale || 'en') as Locale}>
-      <Page page={data.page} />
-    </Wrapper>
-  );
+  return <Page page={data.page} />;
 }
