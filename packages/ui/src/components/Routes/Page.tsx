504f27e738d133f27050165feffd3d0d7a831268
Philipp Melab <philipp.melab@gmail.com>
2023-12-27 20:29:29 +0000

    refactor(SLB-203): use operations to render pages

--- packages/ui/src/components/Routes/Page.tsx
+++ packages/ui/src/components/Routes/Page.tsx
@@ -1,7 +1,8 @@
-import { PageFragment } from '@custom/schema';
+import { PageListEntryFragment, ViewPageQuery } from '@custom/schema';
 import React from 'react';
 
 import { isTruthy } from '../../utils/isTruthy';
+import { useOperation } from '../../utils/operation';
 import { UnreachableCaseError } from '../../utils/unreachable-case-error';
 import { PageTransition } from '../Molecules/PageTransition';
 import { BlockForm } from '../Organisms/PageContent/BlockForm';
@@ -9,15 +10,16 @@ import { BlockMarkup } from '../Organisms/PageContent/BlockMarkup';
 import { BlockMedia } from '../Organisms/PageContent/BlockMedia';
 import { PageHero } from '../Organisms/PageHero';
 
-export function Page(props: { page: PageFragment }) {
-  return (
+export function Page(props: PageListEntryFragment) {
+  const { data } = useOperation(ViewPageQuery, props);
+  return data?.page ? (
     <PageTransition>
       <div>
-        {props.page.hero ? <PageHero {...props.page.hero} /> : null}
+        {data.page.hero ? <PageHero {...data.page.hero} /> : null}
         <div className="bg-white py-12 px-6 lg:px-8">
           <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
             <div className="mt-10">
-              {props.page?.content?.filter(isTruthy).map((block, index) => {
+              {data.page?.content?.filter(isTruthy).map((block, index) => {
                 switch (block.__typename) {
                   case 'BlockMedia':
                     return <BlockMedia key={index} {...block} />;
@@ -34,5 +36,5 @@ export function Page(props: { page: PageFragment }) {
         </div>
       </div>
     </PageTransition>
-  );
+  ) : null;
 }
