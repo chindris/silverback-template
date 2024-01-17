b651ea79c8320c428330d3769b03a8dfc99e723d
Philipp Melab <philipp.melab@gmail.com>
2024-01-17 10:30:11 +0100

    refactor(SLB-202): use operations for static menus query

--- apps/website/src/layouts/index.tsx
+++ apps/website/src/layouts/index.tsx
@@ -1,27 +1,16 @@
-import { SilverbackPageContext } from '@amazeelabs/gatsby-source-silverback';
+import { graphql, useStaticQuery } from '@amazeelabs/gatsby-plugin-operations';
 import { FrameQuery, registerExecutor } from '@custom/schema';
 import { IntlProvider } from '@custom/ui/intl';
 import { Frame } from '@custom/ui/routes/Frame';
-import { graphql, useStaticQuery, WrapPageElementNodeArgs } from 'gatsby';
 import React, { PropsWithChildren } from 'react';
 
 export default function Layout({
   children,
-  pageContext: { locale },
-}: PropsWithChildren<
-  WrapPageElementNodeArgs<any, SilverbackPageContext>['props']
->) {
-  // TODO: Remove duplication of queries here.
-  const data = useStaticQuery<FrameQuery>(graphql`
-    query Frame {
-      mainNavigation: mainNavigations {
-        ...Navigation
-      }
-      footerNavigation: footerNavigations {
-        ...Navigation
-      }
-    }
-  `);
+  locale,
+}: PropsWithChildren<{
+  locale: string;
+}>) {
+  const data = useStaticQuery(graphql(FrameQuery));
   registerExecutor(FrameQuery, data);
   return (
     <IntlProvider locale={locale || 'en'}>
