2a8a9ff768cbcb7310eae0d99f2655aa8b936f99
Luqmaan Essop <luqmaan.essop@amazeelabs.com>
2024-01-25 12:30:29 +0000

    feat: swap to storybook/test calls

--- packages/ui/src/components/Molecules/Pagination.stories.ts
+++ packages/ui/src/components/Molecules/Pagination.stories.ts
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { userEvent, within } from '@storybook/testing-library';
+import { within, userEvent } from '@storybook/test';
 
 import { Pagination as Component } from './Pagination';
 
