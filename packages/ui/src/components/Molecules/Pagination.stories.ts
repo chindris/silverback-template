6c5fddab3fc052e3d7a7630589f20565cb7cd197
Luqmaan Essop <luqmaan.essop@amazeelabs.com>
2024-01-25 12:42:51 +0000

    chore: fix tests

--- packages/ui/src/components/Molecules/Pagination.stories.ts
+++ packages/ui/src/components/Molecules/Pagination.stories.ts
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { within, userEvent } from '@storybook/test';
+import { userEvent, within } from '@storybook/test';
 
 import { Pagination as Component } from './Pagination';
 
