b779fb7206c7c124de2ea643c3266fb1572d9b67
Philipp Melab <philipp.melab@gmail.com>
2024-01-31 21:11:27 +0100

    refactor(SLB-217): simplify 404 pages and remove language negotiator

--- apps/website/src/pages/404.tsx
+++ apps/website/src/pages/404.tsx
@@ -9,11 +9,6 @@ import { Page } from '@custom/ui/routes/Page';
 import { PageProps } from 'gatsby';
 import React from 'react';
 
-import {
-  LanguageNegotiator,
-  LanguageNegotiatorContent,
-} from '../utils/language-negotiator';
-
 export const query = graphql(NotFoundPageQuery);
 
 function isTruthy<T>(value: T | undefined | null): value is T {
@@ -21,29 +16,29 @@ function isTruthy<T>(value: T | undefined | null): value is T {
 }
 
 export default function Index({ data }: PageProps<typeof query>) {
-  const [loc] = useLocation();
-  data.websiteSettings?.notFoundPage?.translations
-    ?.filter(isTruthy)
-    .forEach(({ ...page }) => {
-      registerExecutor(
-        ViewPageQuery,
-        {
-          pathname: loc.pathname,
-        },
-        {
-          page,
-        },
-      );
-    });
-  return (
-    <LanguageNegotiator defaultLanguage={'en'}>
-      {data.websiteSettings?.notFoundPage?.translations
-        ?.filter(isTruthy)
-        .map(({ locale }) => (
-          <LanguageNegotiatorContent key={locale} language={locale}>
-            <Page />
-          </LanguageNegotiatorContent>
-        ))}
-    </LanguageNegotiator>
+  const [{ pathname }] = useLocation();
+  const prefix = pathname.split('/')[1];
+
+  // Get all translations of the 404 page.
+  const pages =
+    data.websiteSettings?.notFoundPage?.translations?.filter(isTruthy);
+
+  // Attempt to get the one matching the current language,
+  // fall back to english, and eventually pick whatever is there.
+  const page =
+    pages?.filter(({ locale }) => locale === prefix).pop() ||
+    pages?.filter(({ locale }) => locale === 'en').pop() ||
+    pages?.pop();
+
+  // Register the resulting page for the current path name.
+  registerExecutor(
+    ViewPageQuery,
+    {
+      pathname,
+    },
+    {
+      page,
+    },
   );
+  return <Page />;
 }
