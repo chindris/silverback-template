63202eba2f12a975ea147439171e05b91b24f5dc
Philipp Melab <philipp.melab@gmail.com>
2024-02-01 13:08:10 +0100

    fix(SLB-217): localized meta tags on the home page

--- apps/website/src/templates/home.tsx
+++ apps/website/src/templates/home.tsx
@@ -1,10 +1,39 @@
 import { graphql } from '@amazeelabs/gatsby-plugin-operations';
-import { HomePageQuery, registerExecutor } from '@custom/schema';
+import { HomePageQuery, registerExecutor, useLocalized } from '@custom/schema';
 import { HomePage } from '@custom/ui/routes/HomePage';
-import { PageProps } from 'gatsby';
+import { HeadProps, PageProps } from 'gatsby';
 import React from 'react';
 
 export const query = graphql(HomePageQuery);
+
+export function Head({ data }: HeadProps<typeof query>) {
+  const page = useLocalized(data.websiteSettings?.homePage?.translations);
+  return page ? (
+    <>
+      <title>{page.title}</title>
+      {page.metaTags?.map((metaTag, index) => {
+        if (metaTag?.tag === 'meta') {
+          return (
+            <meta
+              key={`meta-${index}`}
+              name={metaTag.attributes?.name || metaTag.attributes?.property}
+              content={metaTag.attributes?.content}
+            />
+          );
+        } else if (metaTag?.tag === 'link') {
+          return (
+            <link
+              key={`link-${index}`}
+              rel={metaTag.attributes?.rel}
+              href={metaTag.attributes?.href}
+            />
+          );
+        }
+        return null;
+      }) || null}
+    </>
+  ) : null;
+}
 
 export default function Index({ data }: PageProps<typeof query>) {
   registerExecutor(HomePageQuery, {}, data);
