a5287a0223ac1fba79de9d762d1e421d39d1ef68
Philipp Melab <philipp.melab@gmail.com>
2024-02-05 08:42:10 +0100

    refactor(SLB-217): implement @route and @loadEntity in Gatsby

--- packages/schema/src/page.ts
+++ packages/schema/src/page.ts
@@ -1,13 +1,44 @@
 import { GraphQLFieldResolver } from 'graphql';
 
 // TODO: Generate typing helpers to make this easier.
-export const pageByPath: GraphQLFieldResolver<
+// TODO: Move these into a shared package that implements Drupals "graphql_directives" for Gatsby.
+
+const internalTypes = ['Site', 'SiteBuildMetadata', 'SitePage', 'SitePlugin'];
+
+export const loadEntity: GraphQLFieldResolver<URL, { nodeModel: any }> = async (
+  url,
+  _,
+  { nodeModel },
+) => {
+  const types = nodeModel.getTypes();
+  for (const type of types) {
+    // Skip Gatsby internal types.
+    if (internalTypes.includes(type)) {
+      continue;
+    }
+    try {
+      const result = await nodeModel.findOne({
+        type,
+        query: { filter: { path: { eq: url.pathname } } },
+      });
+      if (result) {
+        return result;
+      }
+    } catch (e) {
+      // Ignore:
+      // Gatsby breaks when trying to query a field that
+      // does not exist on the current type.
+      // TODO: Probably a better solution would be to always
+      //       restrict "loadEntity" by type. But then we need
+      //       to make that change in Drupal too.
+    }
+  }
+};
+
+export const route: GraphQLFieldResolver<
   undefined,
   { nodeModel: any },
   { path: string }
-> = async (_, { path }, { nodeModel }) => {
-  return await nodeModel.findOne({
-    type: 'Page',
-    query: { filter: { path: { eq: path } } },
-  });
+> = (_, { path }) => {
+  return new URL(path, process.env.NETLIFY_URL || 'https://localhost:8000');
 };
