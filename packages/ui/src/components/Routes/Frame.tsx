b72c3ad4e85b24384f7ae2706d689ae96c9b4bbe
Philipp Melab <philipp.melab@gmail.com>
2024-02-08 14:27:52 +0100

    feat(SLB-269): add language switcher component

--- packages/ui/src/components/Routes/Frame.tsx
+++ packages/ui/src/components/Routes/Frame.tsx
@@ -3,23 +3,26 @@ import { AnimatePresence, useReducedMotion } from 'framer-motion';
 import React, { PropsWithChildren } from 'react';
 import { IntlProvider } from 'react-intl';
 
+import { TranslationsProvider } from '../../utils/translations';
 import { Footer } from '../Organisms/Footer';
 import { Header } from '../Organisms/Header';
 
 export function Frame(props: PropsWithChildren<{}>) {
   return (
     <IntlProvider locale={useLocale()}>
-      <Header />
-      <main>
-        {useReducedMotion() ? (
-          <>{props.children}</>
-        ) : (
-          <AnimatePresence mode="wait" initial={false}>
-            {props.children}
-          </AnimatePresence>
-        )}
-      </main>
-      <Footer />
+      <TranslationsProvider>
+        <Header />
+        <main>
+          {useReducedMotion() ? (
+            <>{props.children}</>
+          ) : (
+            <AnimatePresence mode="wait" initial={false}>
+              {props.children}
+            </AnimatePresence>
+          )}
+        </main>
+        <Footer />
+      </TranslationsProvider>
     </IntlProvider>
   );
 }
