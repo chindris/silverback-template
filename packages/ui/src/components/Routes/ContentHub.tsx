2c185c4e613d0f76c83a20e106ad016166508416
Philipp Melab <philipp.melab@gmail.com>
2024-02-08 14:28:35 +0100

    feat(SLB-269): initialize the language switcher from routes

--- packages/ui/src/components/Routes/ContentHub.tsx
+++ packages/ui/src/components/Routes/ContentHub.tsx
@@ -1,9 +1,17 @@
+import { Locale } from '@custom/schema';
 import React from 'react';
 
+import { useTranslations } from '../../utils/translations';
 import { PageTransition } from '../Molecules/PageTransition';
 import { ContentHub as ContentHubOrganism } from '../Organisms/ContentHub';
 
 export function ContentHub(props: { pageSize: number }) {
+  // Initialize the content hub in each language.
+  useTranslations(
+    Object.fromEntries(
+      Object.values(Locale).map((locale) => [locale, `/${locale}/content-hub`]),
+    ),
+  );
   return (
     <PageTransition>
       <ContentHubOrganism pageSize={props.pageSize} />
