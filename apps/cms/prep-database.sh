ccb4a12097d666aca94fe0a7d93465a263c7bb92
Philipp Melab <philipp.melab@gmail.com>
2024-02-10 07:16:13 +0100

    feat(SLB-269): import translations when prepping database

--- apps/cms/prep-database.sh
+++ apps/cms/prep-database.sh
@@ -1,13 +1,13 @@
-if [ ! -z $LAGOON ]
-then
+if [ ! -z $LAGOON ]; then
   # Do not touch database on Lagoon
   exit 0
 fi
 
-if ! test -f web/sites/default/files/.sqlite
-then
+if ! test -f web/sites/default/files/.sqlite; then
   pnpm drupal-install
   pnpm export-webforms
-else
-  pnpm drush cr
 fi
+
+# In any case, re-import translation string.
+pnpm import-translations
+pnpm drush cr
