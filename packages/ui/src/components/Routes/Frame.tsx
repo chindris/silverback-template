da0e975ba0a9010724b576a90bfb0567d71b3f76
Philipp Melab <philipp.melab@gmail.com>
2024-02-13 21:32:33 +0100

    feat(SLB-269): inject translation strings into ui

--- packages/ui/src/components/Routes/Frame.tsx
+++ packages/ui/src/components/Routes/Frame.tsx
@@ -1,15 +1,53 @@
-import { useLocale } from '@custom/schema';
+import { FrameQuery, Locale, useLocale } from '@custom/schema';
 import { AnimatePresence, useReducedMotion } from 'framer-motion';
 import React, { PropsWithChildren } from 'react';
 import { IntlProvider } from 'react-intl';
 
+import translationSources from '../../../build/translatables.json';
+import { useOperation } from '../../utils/operation';
 import { TranslationsProvider } from '../../utils/translations';
 import { Footer } from '../Organisms/Footer';
 import { Header } from '../Organisms/Header';
 
+function filterByLocale(locale: Locale) {
+  return (str: FrameQuery['stringTranslations'][number]) =>
+    str.locale === locale;
+}
+
+function translationsMap(translatables: FrameQuery['stringTranslations']) {
+  return Object.fromEntries(
+    translatables
+      .filter((tr) => tr.translation)
+      .map((tr) => [tr.source, tr.translation]),
+  );
+}
+
+function useTranslations() {
+  const locale = useLocale();
+  const translations = useOperation(FrameQuery).data?.stringTranslations;
+  console.log('out', translations);
+  return {
+    ...translationsMap(translations?.filter(filterByLocale('en')) || []),
+    ...translationsMap(translations?.filter(filterByLocale(locale)) || []),
+  };
+}
+
 export function Frame(props: PropsWithChildren<{}>) {
+  const locale = useLocale();
+  const translations = useTranslations();
+  const messages = Object.fromEntries(
+    Object.keys(translationSources).map((key) => [
+      key,
+      translations[
+        translationSources[key as keyof typeof translationSources]
+          .defaultMessage
+      ] ||
+        translationSources[key as keyof typeof translationSources]
+          .defaultMessage,
+    ]),
+  );
   return (
-    <IntlProvider locale={useLocale()}>
+    <IntlProvider locale={locale} messages={messages}>
       <TranslationsProvider>
         <Header />
         <main>
