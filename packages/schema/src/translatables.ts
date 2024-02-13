b092e0c5861fffd0db4dd4442efe61e3d17c9cbd
Philipp Melab <philipp.melab@gmail.com>
2024-02-13 21:28:25 +0100

    feat(SLB-269): graphql schema for translatable strings

---
+++ packages/schema/src/translatables.ts
@@ -0,0 +1,6 @@
+import { TranslatableStringSource } from './generated/source';
+export function mergeTranslatableStrings(
+  strings: Array<TranslatableStringSource>,
+): Array<TranslatableStringSource> {
+  return strings;
+}
