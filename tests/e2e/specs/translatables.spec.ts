9379a3f09c0be8645adee85cec68c03b8428364e
Philipp Melab <philipp.melab@gmail.com>
2024-02-13 21:33:39 +0100

    test(SLB-269): integration tests for translation strings

--- tests/e2e/specs/translatables.spec.ts
+++ tests/e2e/specs/translatables.spec.ts
@@ -5,11 +5,11 @@ import { websiteUrl } from '../helpers/url';
 test('translatable strings', async ({ page }) => {
   await page.goto(websiteUrl('/en'));
   const footer = page.getByRole('contentinfo');
-  expect(
+  await expect(
     footer.getByText('© 2024 Random Company. All rights reserved.'),
   ).toBeVisible();
   await page.getByRole('link', { name: 'de' }).click();
-  expect(
+  await expect(
     footer.getByText('© 2024 Random Company. Alle Rechte vorbehalten.'),
   ).toBeVisible();
 });
