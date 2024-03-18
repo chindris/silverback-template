3e88d8cd5f0e286084ce298af562b8fc463f5b75
Alex Tkachev <alex.leksat@gmail.com>
2024-03-18 21:48:15 +0400

    chore: make astro somewhat working

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,8 +1,18 @@
 import { defineConfig } from 'astro/config';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@amazeelabs/bridge': path.resolve(
+          'node_modules/@amazeelabs/bridge-astro/build/src/index.js',
+        ),
+      },
+    },
+  },
 });
---
+++ apps/astro/src/pages.ts
@@ -0,0 +1,55 @@
+import { FrameQuery, registerExecutor, ViewPageQuery } from '@custom/schema';
+import operations from '@custom/schema/operations';
+import type { GetStaticPaths } from 'astro';
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // AXXX All fields are null at the moment. The resolvers are only implemented
+  //  in Gatsby.
+  registerExecutor(FrameQuery, await query(operations[FrameQuery]));
+
+  const {
+    data: { pages },
+  }: {
+    data: {
+      pages: Array<{
+        translations: Array<{ path: string }>;
+      }>;
+    };
+  } = await query(`
+    {
+      pages: _queryDrupalPages(offset: 0, limit: 99999999) {
+        translations {
+          path
+        }
+      }
+    }
+  `);
+
+  return pages.flatMap((page) =>
+    page.translations.map((translation) => ({
+      params: { path: translation.path },
+    })),
+  );
+};
+
+export const fetchPageData = async (path: string) => {
+  const res = await query(operations[ViewPageQuery], { pathname: path });
+  console.log('axxx res', res); // AXXX
+  registerExecutor(ViewPageQuery, { pathname: path }, res.data);
+};
+
+async function query(query: string, variables?: any): Promise<any> {
+  return (
+    await fetch('http://127.0.0.1:8888/graphql', {
+      method: 'POST',
+      headers: {
+        'api-key': 'cfdb0555111c0f8924cecab028b53474',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    })
+  ).json();
+}
