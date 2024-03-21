e8d0fd20a28eed38da802fde919d4c051fc2d8bb
Alex Tkachev <alex.leksat@gmail.com>
2024-03-21 16:29:43 +0400

    chore: make astro work after "pnpm deploy"

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -12,6 +12,15 @@ export default defineConfig({
         '@amazeelabs/bridge': path.resolve(
           'node_modules/@amazeelabs/bridge-astro/build/src/index.js',
         ),
+        // AXXX Vite treats monorepo dependencies as source code and bundles
+        //  them. But once we "pnpm deploy" the app, they are not bundled, our
+        //  @custom/schema package is loaded as is, and it imports
+        //  @amazeelabs/bridge. So no replacement to @amazeelabs/bridge-astro
+        //  happens.
+        //  This workaround tricks Vite into treating the listed dependencies as
+        //  source code.
+        '@custom/ui': path.resolve('node_modules/@custom/ui/build/components'),
+        '@custom/schema': path.resolve('node_modules/@custom/schema/build'),
       },
     },
   },
