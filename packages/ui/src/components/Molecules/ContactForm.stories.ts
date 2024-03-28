a445e4208c88a87ac68e6a67fdcfad77e2a7cdcd
Vasile Chindris <vasile.chindris@amazee.com>
2024-03-28 17:12:42 +0200

    feat(SLB-218): remove the ui contact components

--- packages/ui/src/components/Molecules/ContactForm.stories.ts
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react';
-
-import { ContactForm as Component } from './ContactForm';
-
-export default {
-  component: Component,
-} satisfies Meta<typeof Component>;
-
-export const Empty = {} satisfies StoryObj<typeof Component>;
