db65dc09c6d8446ca78ed04a243ceaf9c5190821
Mattia Simonato <mattiasimonato.w@gmail.com>
2024-04-11 16:50:52 +0200

    feat(SLB-304): implement form section into PageHero

--- packages/ui/src/components/Organisms/PageHero.tsx
+++ packages/ui/src/components/Organisms/PageHero.tsx
@@ -1,9 +1,15 @@
 import { Image, Link, PageFragment } from '@custom/schema';
 import React from 'react';
 
+import { BlockForm } from './PageContent/BlockForm';
+
 export function PageHero(props: NonNullable<PageFragment['hero']>) {
+  return props.formUrl ? <FormHero {...props} /> : <DefaultHero {...props} />;
+}
+
+function DefaultHero(props: NonNullable<PageFragment['hero']>) {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <section className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
       {props.image ? (
         <Image
           alt={props.image.alt}
@@ -57,6 +63,50 @@ export function PageHero(props: NonNullable<PageFragment['hero']>) {
           ) : null}
         </div>
       </div>
-    </div>
+    </section>
+  );
+}
+
+function FormHero(props: NonNullable<PageFragment['hero']>) {
+  return (
+    <section>
+      <div className="relative isolate overflow-hidden bg-gray-900 py-24">
+        {props.image ? (
+          <Image
+            alt={props.image.alt}
+            source={props.image.source}
+            priority={true}
+            className="absolute inset-0 -z-10 h-full w-full object-cover"
+            data-test-id={'hero-image'}
+          />
+        ) : null}
+
+        <div className="px-4 lg:pt-24 pt-8 pb-72 lg:pb-80 mx-auto max-w-screen-sm text-center lg:px-6 ">
+          <h1 className="mb-4 text-4xl tracking-tight font-extrabold text-white">
+            {props.headline}
+          </h1>
+          {props.lead ? (
+            <p className="mt-6 text-lg leading-4 text-gray-300">{props.lead}</p>
+          ) : null}
+          {props.ctaText && props.ctaUrl ? (
+            <Link
+              href={props.ctaUrl}
+              className={
+                'mt-7 inline-block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+              }
+            >
+              {props.ctaText}
+            </Link>
+          ) : null}
+        </div>
+      </div>
+      {props.formUrl ? (
+        <div className="px-4 mx-auto -mt-[22rem] lg:-mt-96 max-w-screen-xl lg:px-6 relative">
+          <div className="p-6 mx-auto max-w-screen-md bg-white rounded-lg border border-gray-200 shadow-sm">
+            <BlockForm url={props.formUrl} />
+          </div>
+        </div>
+      ) : null}
+    </section>
   );
 }
