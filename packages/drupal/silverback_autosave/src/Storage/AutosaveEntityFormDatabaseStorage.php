c8c8276fa70ef68ff4a9cdd2932b257e4dedc7d0
Dimitris Spachos <dimitris.spachos@amazeelabs.com>
2024-04-12 11:21:34 +0300

    feat(slb-146): revert purge of previous states (build error)

--- packages/drupal/silverback_autosave/src/Storage/AutosaveEntityFormDatabaseStorage.php
+++ packages/drupal/silverback_autosave/src/Storage/AutosaveEntityFormDatabaseStorage.php
@@ -58,7 +58,7 @@ class AutosaveEntityFormDatabaseStorage implements AutosaveEntityFormStorageInte
       // e.g. when user is navigating away from the page.
       // Also, this approach ensures that there will be always
       // a stored form state in the db, in case we need to restore.
-      $this->purgeAutosavedEntityState($entity_type_id, $entity_id, NULL, $form_id, $langcode, $uid);
+      // $this->purgeAutosavedEntityState($entity_type_id, $entity_id, NULL, $form_id, $langcode, $uid);
       // Now, enter the new values.
       $this->connection->insert(static::AUTOSAVE_ENTITY_FORM_TABLE)
         ->fields([
@@ -169,9 +169,10 @@ class AutosaveEntityFormDatabaseStorage implements AutosaveEntityFormStorageInte
       ->orderBy('timestamp', 'DESC')
       ->condition('form_id', $form_id);
 
-    if (isset($form_session_id)) {
-      $query->condition('form_session_id', $form_session_id);
-    }
+    // Use this for future restore state.
+    /* if (isset($form_session_id)) {
+    $query->condition('form_session_id', $form_session_id);
+    } */
 
     $query->condition('entity_type_id', $entity_type_id)
       ->condition('entity_id', $entity_id)
