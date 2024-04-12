a7aa166b0f424baead5a1ee129b291f73de3b66b
Philipp Melab <philipp.melab@gmail.com>
2024-04-12 17:13:31 +0200

    fix(SLB-209): remove refetch interval

--- packages/ui/src/utils/operation.ts
+++ packages/ui/src/utils/operation.ts
@@ -19,7 +19,10 @@ export function useOperation<TOperation extends AnyOperationId>(
     // so it does not try to fetch.
     executor instanceof Function ? (arg) => executor(arg[1]) : null,
     variables?.pathname?.indexOf('__preview') > 0
-      ? { suspense: false, refreshInterval: 1000 }
+      ? {
+          suspense: false,
+          keepPreviousData: true,
+        }
       : { suspense: false },
   );
   return executor instanceof Function
