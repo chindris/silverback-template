5904b2c99ceb78856746c73f1850c712a61b0cb4
Vasile Chindris <vasile.chindris@amazee.com>
2024-04-16 17:42:21 +0300

    feat(SLB-296): organism placeholder for quote blocks

---
+++ packages/ui/src/components/Organisms/PageContent/BlockQuote.tsx
@@ -0,0 +1,7 @@
+import { BlockQuoteFragment } from '@custom/schema';
+import React from 'react';
+
+export function BlockQuote(props: BlockQuoteFragment) {
+  console.log('Block quote props:', props);
+  return <></>;
+}
