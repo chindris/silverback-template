22cf7dafd3f8d7969e5bd49d8deb87ffae9fba5a
Philipp Melab <philipp.melab@gmail.com>
2024-04-17 08:45:30 +0200

    fix(SLB-209): reconnect websocket on close

--- apps/preview/src/App.tsx
+++ apps/preview/src/App.tsx
@@ -2,6 +2,7 @@ import { OperationExecutor } from '@custom/schema';
 import { Frame } from '@custom/ui/routes/Frame';
 import { Preview, usePreviewRefresh } from '@custom/ui/routes/Preview';
 import { useEffect } from 'react';
+import { retry } from 'rxjs';
 import { webSocket } from 'rxjs/webSocket';
 
 import { drupalExecutor } from './drupal-executor';
@@ -14,7 +15,11 @@ declare global {
 
 const updates$ = webSocket({
   url: `${window.location.origin.replace('http', 'ws')}/__preview`,
-});
+}).pipe(
+  retry({
+    delay: 3000,
+  }),
+);
 
 function App() {
   const refresh = usePreviewRefresh();
