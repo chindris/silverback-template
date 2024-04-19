9cdaf734de1d67c08eccf2e294e3fbb36578d852
Philipp Melab <philipp.melab@gmail.com>
2024-04-19 10:07:19 +0200

    refactor(SLB-222): preload fonts by directory

--- packages/ui/static/public/fonts-async.js
+++
@@ -1,7 +0,0 @@
-function loadFonts(fontFaces) {
-  console.log('hi from font loader')
-  return fontFaces;
-}
-
-// Export the loadFonts function for use in the PostCSS configuration
-module.exports = {loadFonts};
