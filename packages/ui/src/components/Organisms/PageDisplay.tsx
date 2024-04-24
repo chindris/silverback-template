f044a2d6dc83655f95334cd8fe31b7b693556a42
Mattia Simonato <mattiasimonato.w@gmail.com>
2024-04-24 18:06:25 +0200

    fix(SLB-346): reduce vertical spacing between Hero and content

--- packages/ui/src/components/Organisms/PageDisplay.tsx
+++ packages/ui/src/components/Organisms/PageDisplay.tsx
@@ -15,54 +15,52 @@ export function PageDisplay(page: PageFragment) {
     <PageTransition>
       <div>
         {page.hero ? <PageHero {...page.hero} /> : null}
-        <div className="bg-white py-12 px-6 lg:px-8">
+        <div className="bg-white pt-5 pb-12 lg:px-8">
           <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-            <div className="mt-10">
-              {page?.content?.filter(isTruthy).map((block, index) => {
-                switch (block.__typename) {
-                  case 'BlockMedia':
-                    return <BlockMedia key={index} {...block} />;
-                  case 'BlockMarkup':
-                    return <BlockMarkup key={index} {...block} />;
-                  case 'BlockForm':
-                    return <BlockForm key={index} {...block} />;
-                  case 'BlockImageTeasers':
-                    return (
-                      // TODO: Implement BlockImageTeasers
-                      <div
-                        style={{
-                          color: 'red',
-                          border: 'solid 3px red',
-                          padding: '3px',
-                          margin: '5px 0',
-                        }}
-                        // eslint-disable-next-line react/jsx-no-literals
-                      >
-                        BlockImageTeasers goes here
-                      </div>
-                    );
-                  case 'BlockCta':
-                    return <BlockCta key={index} {...block} />;
-                  case 'BlockImageWithText':
-                    return (
-                      // TODO: Implement BlockImageWithText
-                      <div
-                        style={{
-                          color: 'red',
-                          border: 'solid 3px red',
-                          padding: '3px',
-                          margin: '5px 0',
-                        }}
-                        // eslint-disable-next-line react/jsx-no-literals
-                      >
-                        BlockImageWithText goes here
-                      </div>
-                    );
-                  default:
-                    throw new UnreachableCaseError(block);
-                }
-              })}
-            </div>
+            {page?.content?.filter(isTruthy).map((block, index) => {
+              switch (block.__typename) {
+                case 'BlockMedia':
+                  return <BlockMedia key={index} {...block} />;
+                case 'BlockMarkup':
+                  return <BlockMarkup key={index} {...block} />;
+                case 'BlockForm':
+                  return <BlockForm key={index} {...block} />;
+                case 'BlockImageTeasers':
+                  return (
+                    // TODO: Implement BlockImageTeasers
+                    <div
+                      style={{
+                        color: 'red',
+                        border: 'solid 3px red',
+                        padding: '3px',
+                        margin: '5px 0',
+                      }}
+                      // eslint-disable-next-line react/jsx-no-literals
+                    >
+                      BlockImageTeasers goes here
+                    </div>
+                  );
+                case 'BlockCta':
+                  return <BlockCta key={index} {...block} />;
+                case 'BlockImageWithText':
+                  return (
+                    // TODO: Implement BlockImageWithText
+                    <div
+                      style={{
+                        color: 'red',
+                        border: 'solid 3px red',
+                        padding: '3px',
+                        margin: '5px 0',
+                      }}
+                      // eslint-disable-next-line react/jsx-no-literals
+                    >
+                      BlockImageWithText goes here
+                    </div>
+                  );
+                default:
+                  throw new UnreachableCaseError(block);
+              }
+            })}
           </div>
         </div>
       </div>
