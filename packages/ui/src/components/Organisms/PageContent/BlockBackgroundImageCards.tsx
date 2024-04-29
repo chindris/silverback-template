0db95250010de3db394eb8fa68a4db81bd8f5ce7
Mattia Simonato <mattiasimonato.w@gmail.com>
2024-04-29 18:25:56 +0200

    feat(SLB-287): add custom class to teaser container

--- packages/ui/src/components/Organisms/PageContent/BlockBackgroundImageCards.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockBackgroundImageCards.tsx
@@ -11,8 +11,8 @@ import { isTruthy } from '../../../utils/isTruthy';
 export function BlockBackgroundImageCards(props: BlockImageTeasers) {
   return (
     // eslint-disable-next-line tailwindcss/no-custom-classname
-    <section className="block-background-image-cards my-16">
-      <div className="py-8 px-4 mx-auto max-w-7xl text-center lg:py-16 lg:px-12">
+    <section className="container-page my-16 block-background-image-cards">
+      <div className="container-content text-center">
         <div className="grid grid-cols-2 gap-2">
           {props.teasers.filter(isTruthy).map((teaser, index) => (
             <BlockBackgroundImageCard key={index} {...teaser} />
@@ -25,10 +25,10 @@ export function BlockBackgroundImageCards(props: BlockImageTeasers) {
 
 export function BlockBackgroundImageCard(props: BlockImageTeaser) {
   return (
-    <div className="p-8 col-span-2 md:col-span-1 text-left h-96 relative bg-gray-900">
+    <div className="p-8 col-span-2 md:col-span-1 text-left h-72 md:h-96 relative bg-gray-900">
       {props.image ? (
         <Image
-          className="object-cover w-full h-96 mb-5 absolute top-0 left-0"
+          className="object-cover w-full h-72 md:h-96 mb-5 absolute top-0 left-0"
           source={props.image.source}
           alt={props.image.alt}
         />
