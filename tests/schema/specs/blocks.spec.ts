9adcce22cf81a645c93668cae2e5f86c9f6a3149
Vasile Chindris <vasile.chindris@amazee.com>
2024-04-29 17:25:04 +0300

    feat(SLB-292): horizontal separator gutenberg block

--- tests/schema/specs/blocks.spec.ts
+++ tests/schema/specs/blocks.spec.ts
@@ -73,6 +73,9 @@ test('Blocks', async () => {
             __typename
           }
         }
+        ... on BlockHorizontalSeparator {
+          __typename
+        }
       }
     }
     {
@@ -107,6 +110,9 @@ test('Blocks', async () => {
               "markup": "
     <p>A standalone paragraph with <strong><em>markup</em></strong> and <a href="/en/architecture" data-type="Content: Basic page" data-id="[numeric]" data-entity-type="node">link</a></p>
     ",
+            },
+            {
+              "__typename": "BlockHorizontalSeparator",
             },
             {
               "__typename": "BlockMedia",
