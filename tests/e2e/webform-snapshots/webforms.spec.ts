e1cfe2bfc00541bcf843ac1bbfda37a9006bc92a
Alex Tkachev <alex.leksat@gmail.com>
2024-05-01 22:38:25 +0400

    chore(SLB-272): extend styling webform with terms-of-service components

--- tests/e2e/webform-snapshots/webforms.spec.ts
+++ tests/e2e/webform-snapshots/webforms.spec.ts
@@ -13,9 +13,27 @@ test('Export webforms for styling', async ({ page }) => {
 
   const baseUrl = cmsUrl('/en/form/styling?iframe=true&no_css=true');
 
+  page.setViewportSize({
+    // In the frontend the form is wrapped into a div with max-w-3xl class,
+    // which translates to 48 rem, which is 768 px. Roughly.
+    // We try to match the width because the modal is centered dynamically by
+    // Drupal JS, but then, when we save the page, the size and position are
+    // fixed in the HTML.
+    width: 768,
+    // Does not matter.
+    height: 768,
+  });
+
   await page.goto(baseUrl);
   await expect(page.locator(iframeResizerSelector)).toHaveCount(1);
   await savePage(page, 'idle');
+
+  // Disable frontend validation.
+  await page.evaluate(() => {
+    document.querySelectorAll('[required]').forEach((element) => {
+      element.removeAttribute('required');
+    });
+  });
 
   await page.getByLabel('Email with a confirmation').fill('asd@asd');
   await page.getByRole('button', { name: 'Submit' }).click();
@@ -24,6 +42,24 @@ test('Export webforms for styling', async ({ page }) => {
   ).toBeVisible();
   await expect(page.locator(iframeResizerSelector)).toHaveCount(1);
   await savePage(page, 'error');
+
+  await page.goto(baseUrl);
+  await page
+    .locator('label')
+    .filter({ hasText: 'I agree to the terms of service in modal' })
+    .getByRole('button')
+    .click();
+  await page.waitForTimeout(500);
+  await savePage(page, 'terms-of-service-modal');
+
+  await page.goto(baseUrl);
+  await page
+    .locator('label')
+    .filter({ hasText: 'I agree to the terms of service in slideout' })
+    .getByRole('button')
+    .click();
+  await page.waitForTimeout(500);
+  await savePage(page, 'terms-of-service-slideout');
 });
 
 async function savePage(page: Page, name: string) {
