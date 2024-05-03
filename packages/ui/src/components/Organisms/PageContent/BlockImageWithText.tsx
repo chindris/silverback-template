28e7ba37b6c30c4cdb92da3be41857e6ada7601f
HagerDakroury <hagerdakroury@gmail.com>
2024-05-03 10:38:56 +0300

    chore(SLB-288): seperate list styles

--- packages/ui/src/components/Organisms/PageContent/BlockImageWithText.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockImageWithText.tsx
@@ -9,28 +9,29 @@ import React from 'react';
 import { BlockMarkup } from './BlockMarkup';
 
 export function BlockImageWithText(props: BlockImageWithTextFragment) {
-  console.log(props);
   return (
-    <div
-      className={clsx(
-        'flex flex-col lg:flex-row gap-2 lg:gap-16 items-start lg:items-center container-page',
-        {
-          'lg:flex-row-reverse': props.imagePosition === ImagePosition.Right,
-        },
-      )}
-    >
-      {!!props.image?.source && (
-        <div className={'lg:w-1/2 self-start'}>
-          <Image
-            className="object-cover w-full"
-            source={props.image.source}
-            alt={props.image.alt || ''}
-          />
-        </div>
-      )}
+    <div className="container-page my-12 lg:my-16">
+      <div
+        className={clsx(
+          'flex flex-col lg:flex-row gap-2 lg:gap-16 items-start lg:items-center',
+          {
+            'lg:flex-row-reverse': props.imagePosition === ImagePosition.Right,
+          },
+        )}
+      >
+        {!!props.image?.source && (
+          <div className={'lg:w-1/2 self-start'}>
+            <Image
+              className="object-cover w-full"
+              source={props.image.source}
+              alt={props.image.alt || ''}
+            />
+          </div>
+        )}
 
-      <div className={'lg:w-1/2'}>
-        {props.textContent?.markup && <BlockMarkup {...props.textContent} />}
+        <div className={'lg:w-1/2'}>
+          {props.textContent?.markup && <BlockMarkup {...props.textContent} />}
+        </div>
       </div>
     </div>
   );
