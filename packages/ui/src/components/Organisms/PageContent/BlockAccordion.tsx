46520712f9ab7548a26f47bc772ef8de8d98b847
Christophe Jossart <christophe.jossart@amazeelabs.com>
2024-05-03 19:05:16 +0200

    refactor(SLB-301): use custom theme

--- packages/ui/src/components/Organisms/PageContent/BlockAccordion.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockAccordion.tsx
@@ -1,67 +1,85 @@
-import {
-  BlockAccordionFragment,
-  BlockAccordionItemTextFragment,
-} from '@custom/schema';
+import { BlockAccordionFragment } from '@custom/schema';
 import {
   ArrowRightCircleIcon,
   CheckCircleIcon,
   QuestionMarkCircleIcon,
 } from '@heroicons/react/20/solid';
+import { Accordion, CustomFlowbiteTheme, Flowbite } from 'flowbite-react';
 import React from 'react';
 
 import { BlockMarkup } from './BlockMarkup';
 
+const accordionTheme: CustomFlowbiteTheme['accordion'] = {
+  root: {
+    base: 'divide-y divide-gray-200 border-gray-200 dark:divide-gray-700 dark:border-gray-700',
+    flush: {
+      off: 'border-b',
+      on: 'border-b',
+    },
+  },
+  content: {
+    base: 'p-2 m-0 text-gray-200 dark:bg-gray-900',
+  },
+  title: {
+    arrow: {
+      base: 'h-0 w-0',
+    },
+    base: 'flex w-full items-center justify-between p-5 text-left font-medium text-gray-500 dark:text-gray-400',
+    flush: {
+      off: 'hover:bg-gray-100 dark:hover:bg-gray-800 dark:focus:ring-gray-800',
+      on: 'bg-transparent dark:bg-transparent',
+    },
+    heading: '',
+    open: {
+      off: '',
+      on: 'text-gray-900 dark:text-gray-100',
+    },
+  },
+};
+
+// Applying the custom theme to the Accordion component
+// doesn't work out, wrapping it in a Flowbite component.
+const theme: CustomFlowbiteTheme = {
+  accordion: accordionTheme,
+};
+
 export function BlockAccordion(props: BlockAccordionFragment) {
   return (
-    <div data-accordion="open">
-      {props.items.map((item, index) => (
-        <AccordionItemText key={index} id={index} {...item} />
-      ))}
-    </div>
-  );
-}
-
-function AccordionItemText(
-  props: BlockAccordionItemTextFragment & {
-    id: number;
-  },
-) {
-  return (
-    <>
-      <h2 id={`accordion-open-heading-${props.id}`}>
-        <button
-          type="button"
-          className="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
-          data-accordion-target={`#accordion-open-body-${props.id}`}
-          aria-expanded="true"
-          aria-controls={`accordion-open-body-${props.id}`}
-        >
-          <span className="flex items-center">
-            {props.icon && <AccordionIcon icon={props.icon} />} {props.title}
-          </span>
-        </button>
-      </h2>
-      <div
-        id={`accordion-open-body-${props.id}`}
-        className="hidden"
-        aria-labelledby="accordion-open-heading-1"
-      >
-        <div className="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-          {props.textContent?.markup && <BlockMarkup {...props.textContent} />}
-        </div>
-      </div>
-    </>
+    <Flowbite theme={{ theme }}>
+      <Accordion>
+        {props.items.map((item, index) => (
+          <Accordion.Panel key={index}>
+            <Accordion.Title>
+              <span className="flex items-center">
+                {item.icon && <AccordionIcon icon={item.icon} />} {item.title}
+              </span>
+            </Accordion.Title>
+            <Accordion.Content>
+              {item.textContent?.markup && (
+                <BlockMarkup {...item.textContent} />
+              )}
+            </Accordion.Content>
+          </Accordion.Panel>
+        ))}
+      </Accordion>
+    </Flowbite>
   );
 }
 
 function AccordionIcon({ icon }: { icon: string }) {
   switch (icon) {
     case 'questionmark':
-      return <QuestionMarkCircleIcon className="w-5 h-5 me-2 shrink-0" />;
+      return (
+        <QuestionMarkCircleIcon className="w-5 h-5 me-2 shrink-0 text-gray-500" />
+      );
     case 'checkmark':
-      return <CheckCircleIcon className="w-5 h-5 me-2 shrink-0" />;
+      return (
+        <CheckCircleIcon className="w-5 h-5 me-2 shrink-0 text-gray-500" />
+      );
     case 'arrow':
-      return <ArrowRightCircleIcon className="w-5 h-5 me-2 shrink-0" />;
+      return (
+        <ArrowRightCircleIcon className="w-5 h-5 me-2 shrink-0 text-gray-500" />
+      );
     default:
       return null;
   }
