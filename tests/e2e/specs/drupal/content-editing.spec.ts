7845458f6d0bd5a0e28998b3b0ef3e423b28dd2a
Vasile Chindris <vasile.chindris@amazee.com>
2024-05-07 15:43:57 +0300

    test(SLB-328): update e2e tests

--- tests/e2e/specs/drupal/content-editing.spec.ts
+++ tests/e2e/specs/drupal/content-editing.spec.ts
@@ -6,19 +6,21 @@ test.describe('content-editing', () => {
   test.use({ storageState: '.auth/admin.json' });
 
   test('moderation controls are placed in the sidebar', async ({ page }) => {
-    await page.goto(cmsUrl('/node/add/page'));
+    await page.goto(cmsUrl('/drupal'));
+    await page.locator('li.tabs__tab a:text("Edit")').click();
     await expect(
-      page.locator('[aria-label="Editor settings"]').getByText('Save as'),
+      page.locator('[aria-label="Editor settings"]').getByText('Change to'),
     ).toBeVisible();
   });
 
   test('"More settings" fieldset is removed', async ({ page }) => {
+    await page.goto(cmsUrl('/drupal'));
+    await page.locator('li.tabs__tab a:text("Edit")').click();
     // Why we expect it to be removed:
     // - It's too long to scroll to the bottom of long pages
     // - If we have any valuable controls in the "More settings" fieldset,
     //   we should move them to the sidebar, where they are much easier to
     //   access
-    await page.goto(cmsUrl('/node/add/page'));
     await expect(page.locator(':text-is("More settings")')).toHaveCount(0);
   });
 });
