363b4cd6f6601b23aaee182bdc97822d4896f9b3
HagerDakroury <hagerdakroury@gmail.com>
2024-05-14 00:01:54 +0300

    chore(SLB-288): move container containing to each block

--- packages/ui/src/components/Organisms/PageContent/BlockCta.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockCta.tsx
@@ -10,18 +10,20 @@ import React from 'react';
 
 export function BlockCta(props: BlockCtaFragment) {
   return (
-    <Link
-      className={clsx(
-        { 'flex-row-reverse': props.iconPosition === CtaIconPosition.Before },
-        'text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg  py-2 px-3 gap-2 flex flex-row items-center text-xs font-medium text-center w-fit transition-all duration-200 ease-in-out group',
-      )}
-      href={props.url ?? ('/' as Url)}
-      target={props.openInNewTab ? '_blank' : '_self'}
-      rel="noreferrer"
-    >
-      {props.text}
-      {!!props.icon && props.icon === CtaIconType.Arrow && <ArrowRightIcon />}
-    </Link>
+    <div className="container-content">
+      <Link
+        className={clsx(
+          { 'flex-row-reverse': props.iconPosition === CtaIconPosition.Before },
+          'text-blue-600 hover:text-white border border-blue-600 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg  py-2 px-3 gap-2 flex flex-row items-center text-xs font-medium text-center w-fit transition-all duration-200 ease-in-out group',
+        )}
+        href={props.url ?? ('/' as Url)}
+        target={props.openInNewTab ? '_blank' : '_self'}
+        rel="noreferrer"
+      >
+        {props.text}
+        {!!props.icon && props.icon === CtaIconType.Arrow && <ArrowRightIcon />}
+      </Link>
+    </div>
   );
 }
 
--- packages/ui/src/components/Organisms/PageDisplay.tsx
+++ packages/ui/src/components/Organisms/PageDisplay.tsx
@@ -17,8 +17,8 @@ export function PageDisplay(page: PageFragment) {
     <PageTransition>
       <div>
         {page.hero ? <PageHero {...page.hero} /> : null}
-        <div className="bg-white pt-5 pb-12 lg:px-8">
-          <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
+        <div className="bg-white pt-5 pb-12 lg:px-8 container-page">
+          <div className="text-base leading-7 text-gray-700">
             {page?.content?.filter(isTruthy).map((block, index) => {
               switch (block.__typename) {
                 case 'BlockMedia':
