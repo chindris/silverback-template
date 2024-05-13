c748c49f01ebe603b17d2017447a4d3b1c892342
Christophe Jossart <christophe.jossart@amazeelabs.com>
2024-05-13 17:32:05 +0200

    test(SLB-299): use more content

--- packages/ui/src/components/Organisms/PageContent/BlockAccordion.stories.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockAccordion.stories.tsx
@@ -15,7 +15,8 @@ export const AccordionItemText = {
         icon: '',
         textContent: {
           markup: `
-          <p>The earliest known recipe for the modern form of cheese fondue comes from a 1699 book published in Zürich, under the name "Käss mit Wein zu kochen" 'to cook cheese with wine'. It calls for grated or cut-up cheese to be melted with wine, and for bread to be dipped in it.</p>
+          <p>The earliest known recipe for the modern form of cheese fondue comes from a 1699 book published in Zürich, under the name "Käss mit Wein zu kochen" 'to cook cheese with wine'.</p>
+          <p>It calls for grated or cut-up cheese to be melted with wine, and for bread to be dipped in it. However, the name "cheese fondue", until the late 19th century, referred to a dish composed of eggs and cheese, as in la Chapelle's 1735 Fonduë de Fromage, aux Truffes Fraiches; it was something between scrambled eggs with cheese and a cheese soufflé. Brillat-Savarin wrote in 1834 that it is "nothing other than scrambled eggs with cheese". Variations included cream ("à la genevoise") and truffles ("à la piémontaise") in addition to eggs, as well as what is now called "raclette" ("fondue valaisanne").</p>
           <ul>
           <li>Fribourgeoise</li>
           <li>Moitié-Moitié</li>
@@ -29,7 +30,7 @@ export const AccordionItemText = {
         textContent: {
           markup: `
           <p>Rösti is a kind of fried potato cake served as a main course or side dish.</p>
-          <p> As a main dish, rösti is usually accompanied with cheese, onions and cold meat or eggs. This dish, originally from Zürich, was first simply made by frying grated raw potatoes in a pan. It has then spread towards Bern where it is made with boiled potatoes instead. This is where it took the name Rösti. There are many variants in Switzerland and outside the borders. This culinary specialty gives its name to the röstigraben, which designates the cultural differences between the German- and French-speaking parts of the country.</p>
+          <p>As a main dish, rösti is usually accompanied with cheese, onions and cold meat or eggs. This dish, originally from Zürich, was first simply made by frying grated raw potatoes in a pan. It has then spread towards Bern where it is made with boiled potatoes instead. This is where it took the name Rösti. There are many variants in Switzerland and outside the borders. This culinary specialty gives its name to the röstigraben, which designates the cultural differences between the German- and French-speaking parts of the country.</p>
         ` as Markup,
         },
       },
