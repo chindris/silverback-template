ebf36b5eaab6732a8a2904c79fc201fb64eb5b62
Luqmaan Essop <luqmaan.essop@amazeelabs.com>
2024-05-14 07:54:52 +0000

    feat(SLB-226): add a11y checks

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -62,6 +62,28 @@ const SWRCacheDecorator: Decorator = (Story) => {
 
 export const parameters = {
   chromatic: { viewports: [320, 840, 1440] },
+  a11y: {
+    // Optional selector to inspect
+    element: '#storybook-root',
+    config: {
+      rules: [
+        {
+          // The autocomplete rule will not run based on the CSS selector provided
+          id: 'autocomplete-valid',
+          selector: '*:not([autocomplete="nope"])',
+        },
+        {
+          // Setting the enabled option to false will disable checks for this particular rule on all stories.
+          id: 'image-alt',
+          enabled: false,
+        },
+      ],
+    },
+    // Axe's options parameter
+    options: {},
+    // Optional flag to prevent the automatic check
+    manual: true,
+  },
 };
 
 export const decorators = [LocationDecorator, IntlDecorator, SWRCacheDecorator];
