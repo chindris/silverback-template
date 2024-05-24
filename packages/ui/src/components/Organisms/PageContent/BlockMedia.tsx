01525e68f459a788904319a5b6d558dc0a3286d8
Luqmaan Essop <luqmaan.essop@amazee.com>
2024-05-24 16:31:22 +0200

    style(SLB-404): remove scrollpop on media

--- packages/ui/src/components/Organisms/PageContent/BlockMedia.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockMedia.tsx
@@ -2,27 +2,24 @@ import { BlockMediaFragment, Html, Image } from '@custom/schema';
 import React from 'react';
 
 import { UnreachableCaseError } from '../../../utils/unreachable-case-error';
-import { ScrollPop } from '../../Client/ScrollPop';
 
 export function BlockMedia(props: BlockMediaFragment) {
   if (!props.media) {
     return null;
   }
   return (
-    <ScrollPop>
-      <div className="container-page">
-        <div className="container-content">
-          <figure className="mt-16 container-text">
-            <Media {...props.media} />
-            {props.caption ? (
-              <figcaption className="mt-3 flex justify-center gap-x-2 text-sm leading-6 text-gray-500">
-                <Html markup={props.caption} />
-              </figcaption>
-            ) : null}
-          </figure>
-        </div>
+    <div className="container-page">
+      <div className="container-content">
+        <figure className="mt-16 container-text">
+          <Media {...props.media} />
+          {props.caption ? (
+            <figcaption className="mt-3 flex justify-center gap-x-2 text-sm leading-6 text-gray-500">
+              <Html markup={props.caption} />
+            </figcaption>
+          ) : null}
+        </figure>
       </div>
-    </ScrollPop>
+    </div>
   );
 }
 
