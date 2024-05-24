398ff4e2bbee5d09d194543b85e516a1aa993a6f
Mattia Simonato <mattiasimonato.w@gmail.com>
2024-05-24 18:53:03 +0200

    fix: update BlockImageTeasers spacing and LanguageSwitcher style

--- packages/ui/src/components/Molecules/LanguageSwitcher.tsx
+++ packages/ui/src/components/Molecules/LanguageSwitcher.tsx
@@ -37,25 +37,22 @@ export function LanguageSwitcher() {
     <div className="relative inline-block text-left">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button className="inline-flex justify-center w-full rounded-md bg-white text-sm">
+          <Menu.Button className="inline-flex justify-center w-full rounded-md bg-white text-sm hover:text-blue-600">
             {getLanguageName(currentLocale as string)}
-            <ChevronDownIcon
-              className="-mr-1 ml-2 h-5 w-5"
-              aria-hidden="true"
-            />
+            <ChevronDownIcon className="ml-1 h-5 w-5" aria-hidden="true" />
           </Menu.Button>
         </div>
 
         <Transition
           as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-1"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-1"
         >
-          <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 border border-gray-300 rounded-md shadow-lg bg-white">
+          <Menu.Items className="origin-top-right absolute z-50 right-0 mt-3 w-48 rounded bg-white shadow-md ring-1 ring-gray-100">
             <div className="py-1">
               {otherLocales.map((locale) => (
                 <Menu.Item key={locale}>
@@ -64,7 +61,7 @@ export function LanguageSwitcher() {
                       <Link
                         href={translations[locale]!}
                         className={clsx(
-                          focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                          focus ? 'text-blue-600' : 'text-gray-500',
                           'block px-4 py-2 text-sm',
                         )}
                       >
@@ -73,8 +70,8 @@ export function LanguageSwitcher() {
                     ) : (
                       <span
                         className={clsx(
-                          focus ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm',
+                          focus ? 'bg-gray-100 text-gray-900' : 'text-gray-500',
+                          'block px-3.5 py-2 text-sm',
                         )}
                       >
                         {getLanguageName(locale as string)}
