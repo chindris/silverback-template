38d9481db9b88e2f236da93f05f32225a419bf70
Luqmaan Essop <luqmaan.essop@amazeelabs.com>
2024-06-05 13:16:57 +0000

    refactor(SLB-405): enhance reduced motion detection

--- packages/ui/src/components/Molecules/FadeUp.tsx
+++ packages/ui/src/components/Molecules/FadeUp.tsx
@@ -1,13 +1,32 @@
 'use client';
-import { motion, useReducedMotion } from 'framer-motion';
-import React, { PropsWithChildren } from 'react';
+import { motion } from 'framer-motion';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 
 export function FadeUp({
   yGap,
   children,
   className,
 }: PropsWithChildren<{ yGap: number; className?: string }>) {
-  const reducedMotion = useReducedMotion();
+  const [reducedMotion, setReducedMotion] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+
+    const handleChange = () => {
+      setReducedMotion(mediaQuery.matches);
+    };
+
+    // Set the initial value
+    handleChange();
+
+    // Add the event listener
+    mediaQuery.addEventListener('change', handleChange);
+
+    return () => {
+      // Clean up the event listener on unmount
+      mediaQuery.removeEventListener('change', handleChange);
+    };
+  }, [reducedMotion]);
 
   return (
     <motion.div
