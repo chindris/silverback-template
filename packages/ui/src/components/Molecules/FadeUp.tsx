c881d78ae6ee7eacf273f037062f5d4e33d484c6
Luqmaan Essop <luqmaan.essop@amazeelabs.com>
2024-06-05 12:57:42 +0000

    refactor(SLB-405): adjust framer-motion config

--- packages/ui/src/components/Molecules/FadeUp.tsx
+++ packages/ui/src/components/Molecules/FadeUp.tsx
@@ -1,5 +1,4 @@
 'use client';
-import clsx from 'clsx';
 import { motion, useReducedMotion } from 'framer-motion';
 import React, { PropsWithChildren } from 'react';
 
@@ -10,15 +9,11 @@ export function FadeUp({
 }: PropsWithChildren<{ yGap: number; className?: string }>) {
   const reducedMotion = useReducedMotion();
 
-  console.log('reducedMotion:', reducedMotion);
   return (
     <motion.div
-      className={clsx(
-        className,
-        reducedMotion ? 'reducedMotion: On' : 'reducedMotion: Off',
-      )}
+      className={className}
       transition={{ duration: 0.8 }}
-      style={{ opacity: reducedMotion ? 1 : 0, y: reducedMotion ? 0 : yGap }}
+      initial={{ opacity: reducedMotion ? 1 : 0, y: reducedMotion ? 0 : yGap }}
       viewport={{ once: true }}
       whileInView={{ opacity: 1, y: 0 }}
     >
