650149651edbc027e20aaa98a2056149f9060af0
Luqmaan Essop <luqmaan.essop@amazeelabs.com>
2024-07-19 09:30:17 +0000

    chore(SLB:455): update tests

--- tests/e2e/specs/drupal/content-hub.spec.ts
+++ tests/e2e/specs/drupal/content-hub.spec.ts
@@ -21,15 +21,27 @@ test.describe('content hub', () => {
   test('allows to switch pages', async ({ page }) => {
     await page.goto(websiteUrl('/en/content-hub'));
     const content = await page.getByRole('main');
-    const heading = page.getByRole('heading', {
-      name: 'Architecture',
-      level: 5,
-    });
-    await expect(heading).toBeVisible();
+
+    await expect(
+      content.getByRole('heading', {
+        name: 'Architecture',
+        level: 5,
+      }),
+    ).toBeVisible();
     await expect(content.getByText('Gatsby')).not.toBeVisible();
     await content.getByText('Next').click();
-    await expect(heading).not.toBeVisible();
-    await expect(content.getByText('Gatsby')).toBeVisible();
+    await expect(
+      content.getByRole('heading', {
+        name: 'Architecture',
+        level: 5,
+      }),
+    ).not.toBeVisible();
+    await expect(
+      content.getByRole('heading', {
+        name: 'Gatsby',
+        level: 5,
+      }),
+    ).toBeVisible();
   });
 
   test('allows to search for items', async ({ page }) => {
@@ -37,12 +49,18 @@ test.describe('content hub', () => {
     const content = await page.getByRole('main');
     await content.getByPlaceholder('Keyword').fill('technologies');
     await content.getByRole('button', { name: 'Search' }).click();
-    const heading = page.getByRole('heading', {
-      name: 'Architecture',
-      level: 5,
-    });
-    await expect(heading).not.toBeVisible();
-    await expect(content.getByText('Technologies')).toBeVisible();
+    await expect(
+      content.getByRole('heading', {
+        name: 'Architecture',
+        level: 5,
+      }),
+    ).not.toBeVisible();
+    await expect(
+      content.getByRole('heading', {
+        name: 'Technologies',
+        level: 5,
+      }),
+    ).toBeVisible();
   });
 
   test('returns language specific results', async ({ page }) => {
@@ -51,16 +69,23 @@ test.describe('content hub', () => {
     // Change language to German.
     await quickActions.changeLanguageTo(SiteLanguage.Deutsch);
     const content = await page.getByRole('main');
-    const deHeading = page.getByRole('heading', {
-      name: 'Architektur',
-      level: 5,
-    });
-    const enHeading = page.getByRole('heading', {
-      name: 'Architecture',
-      level: 5,
-    });
-    await expect(deHeading).toBeVisible();
-    await expect(enHeading).not.toBeVisible();
-    await expect(content.getByText('Gatsby')).not.toBeVisible();
+    await expect(
+      content.getByRole('heading', {
+        name: 'Architektur',
+        level: 5,
+      }),
+    ).toBeVisible();
+    await expect(
+      content.getByRole('heading', {
+        name: 'Architecture',
+        level: 5,
+      }),
+    ).not.toBeVisible();
+    await expect(
+      content.getByRole('heading', {
+        name: 'Gatsby',
+        level: 5,
+      }),
+    ).not.toBeVisible();
   });
 });
