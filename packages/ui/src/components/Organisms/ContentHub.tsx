212a3ebaadd57c4da14e5b9c0f0e61d29fa12f9d
Luqmaan Essop <luqmaan.essop@amazeelabs.com>
2024-07-19 07:42:27 +0000

    feat(SLB:455): a11y enhancements

--- packages/ui/src/components/Organisms/ContentHub.tsx
+++ packages/ui/src/components/Organisms/ContentHub.tsx
@@ -1,5 +1,11 @@
 import { useIntl } from '@amazeelabs/react-intl';
-import { ContentHubQuery, Image, Link, Locale } from '@custom/schema';
+import {
+  ContentHubQuery,
+  ContentHubResultItemFragment,
+  Image,
+  Link,
+  Locale,
+} from '@custom/schema';
 import qs from 'query-string';
 import React from 'react';
 
@@ -52,43 +58,7 @@ export function ContentHub({ pageSize = 10 }: { pageSize: number }) {
             <ul className="my-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
               {data?.contentHub.items.filter(isTruthy).map((item) => (
                 <li key={item.path}>
-                  <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
-                    <Link href={item.path} className="rounded-t-lg">
-                      {item.teaserImage ? (
-                        <Image {...item.teaserImage} className="w-full" />
-                      ) : (
-                        <div className="aspect-[4/3] bg-indigo-200" />
-                      )}
-                    </Link>
-                    <div className="p-5">
-                      <Link href={item.path}>
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                          {item.title}
-                        </h5>
-                      </Link>
-                      <Link
-                        href={item.path}
-                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                      >
-                        Read more
-                        <svg
-                          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 10"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                        </svg>
-                      </Link>
-                    </div>
-                  </div>
+                  <Card {...item} />
                 </li>
               ))}
             </ul>
@@ -99,3 +69,42 @@ export function ContentHub({ pageSize = 10 }: { pageSize: number }) {
     </div>
   );
 }
+
+export const Card = (item: ContentHubResultItemFragment) => (
+  <article className="focus-within:outline focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600 relative max-w-sm bg-white rounded-lg hover:shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+    <div className="rounded-t-lg">
+      {item.teaserImage ? (
+        <Image {...item.teaserImage} className="w-full" />
+      ) : (
+        <div className="aspect-[4/3] bg-indigo-200" />
+      )}
+    </div>
+    <div className="p-5">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {item.title}
+      </h5>
+      <Link
+        href={item.path}
+        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-blue-700 border border-blue-700 rounded-lg hover:bg-blue-800 hover:text-white focus:outline-offset-4 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 after:content-[''] after:absolute after:inset-0"
+      >
+        <span className="invisible w-0 h-0 overflow-hidden">{item.title} </span>
+        Read more
+        <svg
+          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </Link>
+    </div>
+  </article>
+);
