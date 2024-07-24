4edc29ad5d41b4ee2a72d10675ff4de01e2ca60f
Vasile Chindris <vasile.chindris@amazee.com>
2024-07-24 12:24:08 +0300

    chore(SLB-454): dynamic teaser list content, wip

--- packages/ui/src/components/Organisms/PageContent/BlockTeaserList.tsx
+++ packages/ui/src/components/Organisms/PageContent/BlockTeaserList.tsx
@@ -1,5 +1,18 @@
-import { BlockTeaserListFragment } from '@custom/schema';
+import { useIntl } from '@amazeelabs/react-intl';
+import {
+  BlockTeaserListFragment,
+  Locale,
+  TeaserListQuery,
+} from '@custom/schema';
+import queryString from 'query-string';
 import React from 'react';
+
+import { useOperation } from '../../../utils/operation';
+
+export type TeaserListQueryArgs = {
+  title: string | undefined;
+  pageSize: string | undefined;
+};
 
 export function BlockTeaserList(props: BlockTeaserListFragment) {
   console.log(props);
@@ -13,8 +26,29 @@ export function BlockTeaserList(props: BlockTeaserListFragment) {
           />
         );
       })}
+      {props.contentHubEnabled && <BlockDynamicTeaserList {...props} />}
     </div>
   );
+}
+
+export function BlockDynamicTeaserList(props: BlockTeaserListFragment) {
+  const intl = useIntl();
+  const { data, isLoading, error } = useOperation(TeaserListQuery, {
+    locale: intl.locale as Locale,
+    args: queryString.stringify(
+      {
+        title: props.filters?.title,
+        pageSize: (props.filters?.limit || '0') as string,
+      } satisfies TeaserListQueryArgs,
+      { arrayFormat: 'bracket' },
+    ),
+  });
+  console.log({
+    data,
+    isLoading,
+    error,
+  });
+  return <>Placeholder for dynamic content!</>;
 }
 
 export function BlockTeaserItemContent({ title, hero }: any) {
