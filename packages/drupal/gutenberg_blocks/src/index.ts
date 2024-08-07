33054bd6a0367a03f80a23378512b566db7df355
Dan Lemon <daniel.lemon@amazeelabs.com>
2024-08-07 17:50:11 +0200

    fix: eslint

--- packages/drupal/gutenberg_blocks/src/index.ts
+++ packages/drupal/gutenberg_blocks/src/index.ts
@@ -1,3 +1,22 @@
+import './customisations';
+import './preview';
+import './blocks/hero';
+import './blocks/content';
+import './blocks/heading';
+import './blocks/form';
+import './blocks/image-teaser';
+import './blocks/image-teasers';
+import './blocks/image-with-text';
+import './filters/list';
+import './blocks/cta';
+import './blocks/quote';
+import './blocks/conditional';
+import './blocks/horizontal-separator';
+import './blocks/accordion';
+import './blocks/accordion-item-text';
+import './blocks/info-grid';
+import './blocks/info-grid-item';
+
 declare global {
   const Drupal: {
     behaviors: any;
@@ -19,22 +38,3 @@ declare global {
     };
   };
 }
-
-import './customisations';
-import './preview';
-import './blocks/hero';
-import './blocks/content';
-import './blocks/heading';
-import './blocks/form';
-import './blocks/image-teaser';
-import './blocks/image-teasers';
-import './blocks/image-with-text';
-import './filters/list';
-import './blocks/cta';
-import './blocks/quote';
-import './blocks/conditional';
-import './blocks/horizontal-separator';
-import './blocks/accordion';
-import './blocks/accordion-item-text';
-import './blocks/info-grid';
-import './blocks/info-grid-item';
