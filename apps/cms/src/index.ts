b496212ed492361897a74fb0b740a1dbe3ee5419
Philipp Melab <philipp.melab@gmail.com>
2024-08-14 16:15:51 +0200

    refactor(SLB-445): improve error handling in drupal executor

--- apps/cms/src/index.ts
+++ apps/cms/src/index.ts
@@ -19,38 +19,44 @@ export function createDrupalExecutor(host: string) {
       'X-Forwarded-Host': publicUrl.hostname,
       'X-Forwarded-Port': publicUrl.port,
     };
-    if (isMutation) {
-      const { data, errors } = await (
-        await fetch(url, {
-          method: 'POST',
-          credentials: 'include',
-          body: JSON.stringify({
-            queryId: id,
-            variables: variables || {},
-          }),
-          headers: {
-            ...headers,
-            'Content-Type': 'application/json',
-          },
-        })
-      ).json();
-      if (errors) {
-        throw errors;
-      }
-      return data;
-    } else {
+
+    const requestInit = (
+      isMutation
+        ? {
+            method: 'POST',
+            credentials: 'include',
+            body: JSON.stringify({
+              queryId: id,
+              variables: variables || {},
+            }),
+            headers: {
+              ...headers,
+              'Content-Type': 'application/json',
+            },
+          }
+        : {
+            credentials: 'include',
+            headers,
+          }
+    ) satisfies RequestInit;
+
+    if (!isMutation) {
       url.searchParams.set('queryId', id);
       url.searchParams.set('variables', JSON.stringify(variables || {}));
-      const { data, errors } = await (
-        await fetch(url, {
-          credentials: 'include',
-          headers,
-        })
-      ).json();
+    }
+
+    try {
+      const { data, errors } = await (await fetch(url, requestInit)).json();
       if (errors) {
-        throw errors;
+        console.error('GraphQL error:', errors);
+        if (!data) {
+          throw new Error('GraphQL error: ' + JSON.stringify(errors));
+        }
       }
       return data;
+    } catch (error) {
+      console.error('Fetch error:', error);
+      throw new Error('Fetch error: ' + JSON.stringify(error));
     }
   };
 }
