b0188122836c3f83d9699659e53d47395d32a565
Philipp Melab <philipp.melab@gmail.com>
2024-09-12 07:44:32 +0200

    fix: always use frontend url for graphql requests

--- apps/website/src/executors-client.tsx
+++ apps/website/src/executors-client.tsx
@@ -3,14 +3,14 @@ import { createDrupalExecutor } from '@custom/cms';
 import { OperationExecutorsProvider } from '@custom/schema';
 import React, { PropsWithChildren } from 'react';
 
-import { drupalUrl, frontendUrl } from './utils.js';
+import { frontendUrl } from './utils.js';
 
 export function ClientExecutors({ children }: PropsWithChildren) {
   return (
     <OperationExecutorsProvider
       executors={[
         {
-          executor: createDrupalExecutor(drupalUrl, frontendUrl),
+          executor: createDrupalExecutor(frontendUrl, frontendUrl),
         },
       ]}
     >
