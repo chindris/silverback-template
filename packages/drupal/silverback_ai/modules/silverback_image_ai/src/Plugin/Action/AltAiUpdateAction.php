df95bffa820874e9af168be086fe8991d4450d3e
Dimitris Spachos <dimitris.spachos@amazeelabs.com>
2024-11-15 09:53:19 +0200

    feat(slb-489): add ai-powered alt text generation action for images

--- packages/drupal/silverback_ai/modules/silverback_image_ai/src/Plugin/Action/AltAiUpdateAction.php
+++ packages/drupal/silverback_ai/modules/silverback_image_ai/src/Plugin/Action/AltAiUpdateAction.php
@@ -14,24 +14,36 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
  */
 #[Action(
   id: 'entity:alt_ai_update_action',
-  action_label: new TranslatableMarkup('Alt text update (images only)'),
+  action_label: new TranslatableMarkup('Alt text update (AI)'),
   deriver: EntityPublishedActionDeriver::class
 )]
 class AltAiUpdateAction extends ActionBase {
+
+  private const BUNDLE_IMAGE = 'image';
+  private const DEFAULT_LANGCODE = 'en';
 
   /**
    * {@inheritdoc}
    */
   public function execute($entity = NULL) {
-    // Do something.
-    \Drupal::logger('debug')->debug(__METHOD__);
-    sleep(1);
+    // @todo Add DI
+    if ($entity->bundle() == self::BUNDLE_IMAGE) {
+      $langcode = $entity->langcode->value ?? self::DEFAULT_LANGCODE;
+      $service = \Drupal::service('silverback_image_ai.utilities');
+      $file = $entity->field_media_image->entity;
+      if ($file) {
+        $alt_text = $service->generateImageAlt($file, $langcode);
+        if (!empty($alt_text)) {
+          $service->setMediaImageAltText($entity, $alt_text);
+        }
+      }
+    }
   }
 
   /**
    * {@inheritdoc}
    */
-  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+  public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE) {
     $result = AccessResult::allowedIfHasPermission($account, 'create media');
     return $return_as_object ? $result : $result->isAllowed();
   }
