69b26df8029bd1fd492c30c070bb196f9d74b372
Alex Tkachev <alex.leksat@gmail.com>
2024-12-03 19:31:03 +0400

    feat: add default content export check

--- apps/cms/prep-database.sh
+++ apps/cms/prep-database.sh
@@ -17,3 +17,17 @@ fi
 # In any case, re-import translation string.
 pnpm import-translations
 pnpm drush cr
+
+# On CI, run the content export and check if the export is up to date.
+if [ ! -z $GITHUB_ACTIONS ]; then
+  pnpm content:export
+  if [[ $(git status --porcelain) ]]; then
+    echo "Error: Content export is not up to date"
+    echo "Please run 'pnpm content:export' from apps/cms and commit the changes"
+    echo "Detected changes:"
+    git status --porcelain
+    echo "Diff:"
+    git diff
+    exit 1
+  fi
+fi
