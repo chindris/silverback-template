5fecba2c85efaad8f4811623ebc601dd2bf58f22
Alex Tkachev <alex.leksat@gmail.com>
2024-12-05 17:32:35 +0400

    chore(WIP): try @graphql-eslint/no-unused-fields

--- packages/schema/eslint.config.mjs
+++ packages/schema/eslint.config.mjs
@@ -1,8 +1,27 @@
 import { base, defineConfig } from '@custom/eslint-config';
+import graphqlPlugin from '@graphql-eslint/eslint-plugin';
 
 export default defineConfig([
   ...base,
   {
     ignores: ['src/generated/**', 'build/**'],
+  },
+  {
+    files: ['**/*.graphql'],
+    languageOptions: {
+      parser: graphqlPlugin.parser,
+      parserOptions: {
+        graphQLConfig: {
+          schema: ['./src/schema.graphql', './src/directives.graphql'],
+          documents: '../../tests/schema/specs/*.ts',
+        },
+      },
+    },
+    plugins: {
+      '@graphql-eslint': graphqlPlugin,
+    },
+    rules: {
+      '@graphql-eslint/no-unused-fields': 'warn',
+    },
   },
 ]);
